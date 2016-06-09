1a5da695cf76ddffaa3b4b3ce6a91aec7d2d60c5
witian94 <will940703@gmail.com>
2016-06-09 14:40:18 -0500

    some modify in modelio, parser and test_parser

--- parser.py
+++ parser.py
@@ -1,5 +1,3 @@
-import random
-
 __author__ = 'haotian'
 
 
@@ -37,6 +35,8 @@ def to_digit(x):
     :param x: the input string to be converted
     :return: the result of convert
     """
+    if not isinstance(x, str):
+        return x
     try:
         y = int(x)
         return y
@@ -52,83 +52,82 @@ def to_digit(x):
 
 class Data:
     def __init__(self, features, data):
-        self.features = features
-        self.data = data
-        self.filtered_data = list(data)
-        self.x_features = features[:-1]
-        self.y_feature = features[-1]
-        self.max_min = []
+        self.__features = features
+        self.__data = data
+        self.__filtered_data = list(data)
+        self.__x_features = features[:-1]
+        self.__y_feature = features[-1]
+        self.__max_min = []
         return
 
     def set_x_features(self, feature_list):
+        if isinstance(feature_list, str):
+            feature_list = [feature_list]
         for feature in feature_list:
-            if feature not in self.features:
+            if feature not in self.__features:
                 print("can't find [{}] in features".format(feature))
                 return False
-        self.x_features = feature_list
+        self.__x_features = feature_list
         return True
 
     def set_y_feature(self, feature):
-        if feature not in self.features:
+        if feature not in self.__features:
             print("can't find [] in features".format(feature))
             return False
-        self.y_feature = feature
+        self.__y_feature = feature
         return True
 
     def add_filter(self, feature, operator, threshold):
-        if feature not in self.features:
+        if feature not in self.__features:
             print("can't find [{}] in features".format(feature))
             return False
-        index = self.features.index(feature)
+        index = self.__features.index(feature)
         filtered_data = []
-        for line in self.filtered_data:
+        for line in self.__filtered_data:
             if line[index] > threshold and '>' in operator:
                 filtered_data.append(line)
             elif line[index] == threshold and '=' in operator:
                 filtered_data.append(line)
             elif line[index] < threshold and '<' in operator:
                 filtered_data.append(line)
-        self.filtered_data = filtered_data
+        self.__filtered_data = filtered_data
         return True
 
-    def get_filtered_data(self):
-        return list(self.filtered_data)
-
     def remove_filter(self):
-        self.filtered_data = list(self.data)
+        self.__filtered_data = list(self.__data)
         return True
 
     def normalization(self, features=None, normalization_type='s'):
         if features is None:
-            features = self.x_features
+            features = self.__x_features
         elif isinstance(features, str):
             features = [features]
         for feature in features:
-            if feature not in self.features:
+            if feature not in self.__features:
                 print("can't find [{}] in features".format(feature))
                 return False
-            if self.max_min[0][self.features.index(feature)] is None:
+            if self.__max_min[0][self.__features.index(feature)] is None:
                 print("Feature [{}] is not numerical".format(feature))
                 return False
         if normalization_type == 's':
             for feature in features:
-                index = self.features.index(feature)
-                cur_max = self.max_min[0][index]
-                cur_min = self.max_min[1][index]
-                for line in self.data:
+                index = self.__features.index(feature)
+                cur_max = self.__max_min[0][index]
+                cur_min = self.__max_min[1][index]
+                for line in self.__data:
                     line[index] = (line[index]-cur_min)/(cur_max-cur_min)
-                for line in self.filtered_data:
+                for line in self.__filtered_data:
                     line[index] = (line[index]-cur_min)/(cur_max-cur_min)
         elif normalization_type == 't':
-            all_max = max([self.max_min[0][x] for x in range(len(self.features)) if self.features(x) in features])
-            all_min = min([self.max_min[1][x] for x in range(len(self.features)) if self.features(x) in features])
+            all_max = max([self.__max_min[0][x] for x in range(len(self.__features)) if self.__features(x) in features])
+            all_min = min([self.__max_min[1][x] for x in range(len(self.__features)) if self.__features(x) in features])
             for feature in features:
-                index = self.features.index(feature)
-                self.max_min[0][index] = all_max
-                self.max_min[1][index] = all_min
-                for line in self.data:
+                index = self.__features.index(feature)
+                self.__max_min[0][index] = all_max
+                self.__max_min[1][index] = all_min
+                for line in self.__data:
                     line[index] = (line[index]-all_min)/(all_max-all_min)
-                for line in self.filtered_data:
+                for line in self.__filtered_data:
                     line[index] = (line[index]-all_min)/(all_max-all_min)
         else:
             print("unknown normalization_type '{}'; "
@@ -137,39 +136,39 @@ class Data:
         return True
 
     def unnormalization_data_point(self, feature, data):
-        if feature not in self.features:
+        if feature not in self.__features:
             print("can't find [{}] in features".format(feature))
             return None
-        index = self.features.index(feature)
-        cur_max = self.max_min[0][index]
-        cur_min = self.max_min[1][index]
+        index = self.__features.index(feature)
+        cur_max = self.__max_min[0][index]
+        cur_min = self.__max_min[1][index]
         return data*(cur_max-cur_min)+cur_min
 
     def get_data(self, features=None):
         if features is None:
-            features = self.features
+            features = self.__features
         if isinstance(features, str):
             features = [features]
         for feature in features:
-            if feature not in self.features:
+            if feature not in self.__features:
                 print("can't find [{}] in features".format(feature))
                 return None
         output = []
-        index_list = [i for i in range(len(self.features)) if self.features[i] in features]
-        for line in self.filtered_data:
+        index_list = [i for i in range(len(self.__features)) if self.__features[i] in features]
+        for line in self.__filtered_data:
             output.append([line[i] for i in index_list])
         return output
 
     def get_x_data(self):
-        return self.get_data(features = self.x_features)
+        return self.get_data(features=self.__x_features)
 
     def get_y_data(self):
-        return self.get_data(features = self.y_feature)
+        return self.get_data(features=self.__y_feature)
 
     def __calculate_data_range(self):
-        maxes = list(self.data[0])
-        mins = list(self.data[0])
-        for line in self.data:
+        maxes = list(self.__data[0])
+        mins = list(self.__data[0])
+        for line in self.__data:
             for i in range(len(line)):
                 if isinstance(line[i], str):
                     continue
@@ -177,9 +176,9 @@ class Data:
                     maxes[i] = line[i]
                 elif line[i] < mins[i]:
                     mins[i] = line[i]
-        for i in range(len(self.features)):
+        for i in range(len(self.__features)):
             if isinstance(maxes[i], str):
                 maxes[i] = None
                 mins[i] = None
-        self.max_min.append(maxes)
-        self.max_min.append(mins)
+        self.__max_min.append(maxes)
+        self.__max_min.append(mins)
