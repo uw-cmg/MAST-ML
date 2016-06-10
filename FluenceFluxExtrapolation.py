5b1c8e5619defebc723f8600bfd907f69cfabb68
JeritGeorge <george6@wisc.edu>
2016-06-10 16:07:34 -0500

    added ability to run all tests at once

--- FluenceFluxExtrapolation.py
+++ FluenceFluxExtrapolation.py
@@ -4,60 +4,59 @@ import data_parser
 from sklearn.metrics import mean_squared_error
 from sklearn.kernel_ridge import KernelRidge
 
-def getModel():
-    return KernelRidge(alpha= .00139, coef0=1, degree=3, gamma=.518, kernel='rbf', kernel_params=None)
+def FlFxExt(model = KernelRidge(alpha= .00139, coef0=1, degree=3, gamma=.518, kernel='rbf', kernel_params=None), datapath = "../../DBTT_Data.csv", savepath = '../../{}.png'):
 
-data = data_parser.parse("../../DBTT_Data.csv")
-data.set_x_features(["N(Cu)", "N(Ni)", "N(Mn)", "N(P)", "N(Si)","N( C )", "N(log(fluence)", "N(log(flux)", "N(Temp)"])
-data.set_y_feature("delta sigma")
+    data = data_parser.parse(datapath)
+    data.set_x_features(["N(Cu)", "N(Ni)", "N(Mn)", "N(P)", "N(Si)","N( C )", "N(log(fluence)", "N(log(flux)", "N(Temp)"])
+    data.set_y_feature("delta sigma")
 
-fluence_divisions = [3.3E18, 3.3E19, 3.3E20]
-flux_divisions = [5.3e11, 4e12, 3e13]
+    fluence_divisions = [3.3E18, 3.3E19, 3.3E20]
+    flux_divisions = [5.3e11, 4e12, 3e13]
 
-for x in fluence_divisions:
-    model = getModel()
-    data.remove_filter()
-    data.add_filter("fluence n/cm2", '<', x)
-    l_train = len(data.get_y_data())
-    model.fit(data.get_x_data(), data.get_y_data())
+    for x in fluence_divisions:
+        model = model
+        data.remove_filter()
+        data.add_inclusive_filter("fluence n/cm2", '<', x)
+        l_train = len(data.get_y_data())
+        model.fit(data.get_x_data(), data.get_y_data())
 
-    data.remove_filter()
-    data.add_filter("fluence n/cm2", '>=', x)
-    l_test = len(data.get_y_data())
-    Ypredict = model.predict(data.get_x_data())
-    RMSE = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
+        data.remove_filter()
+        data.add_inclusive_filter("fluence n/cm2", '>=', x)
+        l_test = len(data.get_y_data())
+        Ypredict = model.predict(data.get_x_data())
+        RMSE = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
 
-    plt.scatter(data.get_y_data(), Ypredict, color = 'black')
-    plt.plot(plt.gca().get_ylim(), plt.gca().get_ylim(), ls="--", c=".3")
-    plt.xlabel('Measured ∆sigma (Mpa)')
-    plt.ylabel('Predicted ∆sigma (Mpa)')
-    plt.title('Testing Fluence > {}'.format(x))
-    plt.figtext(.15,.83,'RMSE: {:.3f}'.format(RMSE), fontsize = 14)
-    plt.figtext(.15,.78,'Train: {}, Test: {}'.format(l_train, l_test), fontsize = 13)
+        plt.scatter(data.get_y_data(), Ypredict, color = 'black')
+        plt.plot(plt.gca().get_ylim(), plt.gca().get_ylim(), ls="--", c=".3")
+        plt.xlabel('Measured ∆sigma (Mpa)')
+        plt.ylabel('Predicted ∆sigma (Mpa)')
+        plt.title('Testing Fluence > {}'.format(x))
+        plt.figtext(.15,.83,'RMSE: {:.3f}'.format(RMSE), fontsize = 14)
+        plt.figtext(.15,.78,'Train: {}, Test: {}'.format(l_train, l_test), fontsize = 13)
 
-    plt.savefig('../../{}.png'.format(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
-    plt.show()
+        plt.savefig(savepath.format(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
+        plt.show()
 
-for x in flux_divisions:
-    model = getModel()
-    data.remove_filter()
-    data.add_filter("flux n/cm2/s", '<', x)
-    l_train = len(data.get_y_data())
-    model.fit(data.get_x_data(), data.get_y_data())
+    for x in flux_divisions:
+        model = model
+        data.remove_filter()
+        data.add_filter("flux n/cm2/s", '<', x)
+        l_train = len(data.get_y_data())
+        model.fit(data.get_x_data(), data.get_y_data())
 
-    data.remove_filter()
-    data.add_filter("flux n/cm2/s", '>=', x)
-    l_test = len(data.get_y_data())
-    Ypredict = model.predict(data.get_x_data())
-    RMSE = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
+        data.remove_filter()
+        data.add_filter("flux n/cm2/s", '>=', x)
+        l_test = len(data.get_y_data())
+        Ypredict = model.predict(data.get_x_data())
+        RMSE = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
 
-    plt.scatter(data.get_y_data(), Ypredict, color = 'black')
-    plt.plot(plt.gca().get_ylim(), plt.gca().get_ylim(), ls="--", c=".3")
-    plt.xlabel('Measured ∆sigma (Mpa)')
-    plt.ylabel('Predicted ∆sigma (Mpa)')
-    plt.title('Testing Flux > {:.1E}'.format(x))
-    plt.figtext(.15,.83,'RMSE: {:.2f}'.format(RMSE), fontsize = 14)
-    plt.figtext(.15,.78,'Train: {}, Test: {}'.format(l_train, l_test), fontsize = 13)
+        plt.scatter(data.get_y_data(), Ypredict, color = 'black')
+        plt.plot(plt.gca().get_ylim(), plt.gca().get_ylim(), ls="--", c=".3")
+        plt.xlabel('Measured ∆sigma (Mpa)')
+        plt.ylabel('Predicted ∆sigma (Mpa)')
+        plt.title('Testing Flux > {:.1E}'.format(x))
+        plt.figtext(.15,.83,'RMSE: {:.2f}'.format(RMSE), fontsize = 14)
+        plt.figtext(.15,.78,'Train: {}, Test: {}'.format(l_train, l_test), fontsize = 13)
 
-    plt.savefig('../../{}.png'.format(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
-    plt.show()
+        plt.savefig(savepath.format(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
+        plt.show()
--- LeaveOutAlloyCV.py
+++ LeaveOutAlloyCV.py
@@ -4,42 +4,41 @@ import numpy as np
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_squared_error
 
-def getModel():
-    return KernelRidge(alpha= .00139, coef0=1, degree=3, gamma=.518, kernel='rbf', kernel_params=None)
+def LOACV(model = KernelRidge(alpha= .00139, coef0=1, degree=3, gamma=.518, kernel='rbf', kernel_params=None), datapath = "../../DBTT_Data.csv", savepath = '../../{}.png'): 
+    data = data_parser.parse(datapath)
+    data.set_x_features(["N(Cu)", "N(Ni)", "N(Mn)", "N(P)", "N(Si)","N( C )", "N(log(fluence)", "N(log(flux)", "N(Temp)"])
+    data.set_y_feature("delta sigma")
 
-data = data_parser.parse("../../DBTT_Data.csv")
-data.set_x_features(["N(Cu)", "N(Ni)", "N(Mn)", "N(P)", "N(Si)","N( C )", "N(log(fluence)", "N(log(flux)", "N(Temp)"])
-data.set_y_feature("delta sigma")
+    rms_list = []
+    alloy_list = []
 
-rms_list = []
-alloy_list = []
+    for alloy in range(1,60):
+        model = model #creates a new model
 
-for alloy in range(1,60):
-    model = getModel() #creates a new model
+        #fit model to all alloys except the one to be removed 
+        data.remove_filter()
+        data.add_inclusive_filter("Alloy", '<>', alloy) 
+        model.fit(data.get_x_data(), data.get_y_data())
 
-    #fit model to all alloys except the one to be removed 
-    data.remove_filter()
-    data.add_filter("Alloy", '<>', alloy) 
-    model.fit(data.get_x_data(), data.get_y_data())
+        #predict removed alloy
+        data.remove_filter()
+        data.add_inclusive_filter("Alloy", '=', alloy)
+        if not data.get_x_data(): continue #if alloy doesn't exist(x data is empty), then continue
+        Ypredict = model.predict(data.get_x_data())
 
-    #predict removed alloy
-    data.remove_filter()
-    data.add_filter("Alloy", '=', alloy)
-    if not data.get_x_data(): continue #if alloy doesn't exist(x data is empty), then continue
-    Ypredict = model.predict(data.get_x_data())
+        rms = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
+        rms_list.append(rms)
+        alloy_list.append(alloy)
 
-    rms = np.sqrt(mean_squared_error(Ypredict, data.get_y_data()))
-    rms_list.append(rms)
-    alloy_list.append(alloy)
+    print('Mean RMSE: ', np.mean(rms_list))
 
-print('Mean RMSE: ', np.mean(rms_list))
-
-# graph rmse vs alloy 
-ax = plt.gca()
-plt.scatter(alloy_list,rms_list, color='black')
-plt.plot((0, 59), (0, 0), ls="--", c=".3")
-plt.xlabel('Alloy Number')
-plt.ylabel('RMSE (Mpa)')
-plt.title('Leave out Alloy RMSE')
-plt.savefig('../../%s.png' %(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
-plt.show()
+    # graph rmse vs alloy 
+    ax = plt.gca()
+    plt.scatter(alloy_list,rms_list, color='black')
+    plt.plot((0, 59), (0, 0), ls="--", c=".3")
+    plt.xlabel('Alloy Number')
+    plt.ylabel('RMSE (Mpa)')
+    plt.title('Leave out Alloy RMSE')
+    plt.figtext(.15,.83,'Mean RMSE: {:.2f}'.format(np.mean(rms_list)), fontsize = 14)
+    plt.savefig(savepath.format(plt.gca().get_title()), dpi = 200, bbox_inches='tight')
+    plt.show()
