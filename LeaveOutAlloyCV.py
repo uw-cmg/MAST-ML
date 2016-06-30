189febcff0934703588f2f7a3183e1679f2c2b14
JeritGeorge <george6@wisc.edu>
2016-06-30 13:50:02 -0500

    added annotations to plot

--- LeaveOutAlloyCV.py
+++ LeaveOutAlloyCV.py
@@ -10,8 +10,7 @@ def execute(model, data, savepath):
     rms_list = []
     alloy_list = []
 
-    for alloy in range(1, 60):
-        model = model  # creates a new model
+    for alloy in range(1, 60): 
 
         # fit model to all alloys except the one to be removed
         data.remove_all_filters()
@@ -31,15 +30,16 @@ def execute(model, data, savepath):
     print('Mean RMSE: ', np.mean(rms_list))
 
     # graph rmse vs alloy 
-    ax = plt.gca()
-    plt.figure(figsize=(10, 4))
+    fig, ax = plt.subplots(figsize=(10, 4))
     plt.xticks(np.arange(0, max(alloy_list) + 1, 5))
-    plt.scatter(alloy_list, rms_list, color='black', s=10)
-    plt.plot((0, 59), (0, 0), ls="--", c=".3")
-    plt.xlabel('Alloy Number')
-    plt.ylabel('RMSE (Mpa)')
-    plt.title('Leave out Alloy RMSE')
-    plt.figtext(.15, .83, 'Mean RMSE: {:.2f}'.format(np.mean(rms_list)), fontsize=14)
-    plt.savefig(savepath.format(plt.gca().get_title()), dpi=200, bbox_inches='tight')
-    plt.clf()
+    ax.scatter(alloy_list, rms_list, color='black', s=10)
+    ax.plot((0, 59), (0, 0), ls="--", c=".3")
+    ax.set_xlabel('Alloy Number')
+    ax.set_ylabel('RMSE (Mpa)')
+    ax.set_title('Leave out Alloy RMSE')
+    ax.text(.05, .88, 'Mean RMSE: {:.2f}'.format(np.mean(rms_list)), fontsize=14, transform=ax.transAxes)
+    for x in np.argsort(rms_list)[-5:]:
+        ax.annotate(s = alloy_list[x],xy = (alloy_list[x], rms_list[x]))
+    fig.savefig(savepath.format(ax.get_title()), dpi=200, bbox_inches='tight')
+    fig.clf()
     plt.close()
