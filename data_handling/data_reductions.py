d3f66ffc3dc6d81c96cc6930d3cc0a9c23493e4f
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-01-27 11:59:32 -0600

    Finished major data reductions

--- data_handling/data_reductions.py
+++ data_handling/data_reductions.py
@@ -45,8 +45,10 @@ def update_experimental_temperatures(cname, verbose=1):
             Their CD temperature, however, remains at 290. 
     """ 
     id_list=list()
+    orig_temp_list=list()
     flux=2.30e14 #n/cm2/sec
     fluence=1.10e21 #n/cm2
+    newtemp=320 #degrees C
     num_list=[6,34,35,36,37,38]
     for num in num_list:
         aname = look_up_name_or_number(num, "number")
@@ -55,9 +57,20 @@ def update_experimental_temperatures(cname, verbose=1):
                         "fluence_n_cm2":fluence})
         for result in results:
             id_list.append(result["_id"])
+            orig_temp_list.append(result["temperature_C"])
     #need to do actual temperature modification
-    for rid in id_list:
-        print("%s: needs temperature update" % rid)
+    for modidx in range(0, len(id_list)):
+        db[cname].update_one(
+            {"_id":ObjectId(id_list[modidx])},
+            {
+                "$set":{
+                    "original_reported_temperature_C": orig_temp_list[modidx],
+                    "temperature_C": newtemp
+                }
+            }
+        )
+        if verbose > 0:
+            print("%s: temperature updated" % id_list[modidx])
     return id_list
 
 def look_up_name_or_number(istr="",itype="name", verbose=0):
@@ -145,18 +158,38 @@ def get_duplicate_ids_to_remove(cname, dupcheck="delta_sigma_y_MPa", verbose=1):
             print("%s: %s" % (id_list[iidx], reason_list[iidx]))
     return [id_list, reason_list]
 
+def flag_for_ignore(cname, id_list, reason_list, verbose=1):
+    for fidx in range(0, len(id_list)):
+        flagid = id_list[fidx]
+        db[cname].update_one(
+            {"_id":ObjectId(flagid)},
+            {
+                "$set":{
+                    "ignore":1,
+                    "ignore_reason":reason_list[fidx]
+                }
+            }
+        )
+        if verbose > 0:
+            print("Updated record %s" % flagid)
+    return
+
 def main_exptivar(cname="ucsbivarplus",verbose=1):
     [id_list, reason_list] = get_alloy_removal_ids(cname)
+    flag_for_ignore(cname, id_list, reason_list)
     print(len(id_list))
     [id_list, reason_list] = get_duplicate_ids_to_remove(cname)
+    flag_for_ignore(cname, id_list, reason_list)
     print(len(id_list))
     update_experimental_temperatures(cname)
     return
 
 def main_cdivar(cname="cdivar2017",verbose=1):
     [id_list, reason_list] = get_alloy_removal_ids(cname)
+    flag_for_ignore(cname, id_list, reason_list)
     print(len(id_list))
     [id_list, reason_list] = get_duplicate_ids_to_remove(cname,"CD_delta_sigma_y_MPa") 
+    flag_for_ignore(cname, id_list, reason_list)
     print(len(id_list))
     #   #the same number of duplicate ids should be removed as in the
     #   #experimental case; where CD was run, both the true duplicates and
