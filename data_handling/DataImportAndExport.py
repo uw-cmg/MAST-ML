a2a3c9f17465c0592c8fc34adc85d7e98ba6f2d5
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-02-03 16:51:05 -0600

    Streamline data management process

---
+++ data_handling/DataImportAndExport.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+###################
+# Data import and export for the dbtt database
+# Tam Mayeshiba 2017-02-03
+#
+# This script is intended to be run on a local computer in order to
+# generate .csv files.
+#
+# Prerequisites:
+# 1. Must have mongodb installed and running.
+#    Visit https://docs.mongodb.com/manual/administration/install-community/
+# 2. Must have starting import csv files available.
+#
+###################
+import numpy as np
+import pymongo
+import os
+import sys
+import traceback
+import subprocess
+import data_reductions as dred
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+#Set up paths and names
+dbpath = "../../../data/DBTT_mongo"
+importpath = "../../../data/DBTT_mongo/imports_201702"
+db_base="dbtt"
+db = "" #will be set by script
+ivarpluscname = "ivarplus_data"
+#The following csv files should be in $importpath
+cbasic=dict()
+cbasic["alloys"] = "alloy_properties.csv"
+cbasic["cd_ivar_2017"]="CD_IVAR_Hardening_2017-1_with_ivar_columns_reduced.csv"
+cbasic["cd_ivar_2016"]="CD_IVAR_Hardening_clean_2016.csv"
+cbasic["cd_lwr_2017"]="lwr_cd_2017_reduced_for_import.csv"
+cbasic["ucsb_ivar_and_ivarplus"]="ucsb_ivar_and_ivarplus.csv"
+
+def get_mongo_client():
+    """Check connection and get mongo client
+        Based on http://stackoverflow.com/questions/30539183/how-do-you-check-if-the-client-for-a-mongodb-instance-is-valid
+    """
+    timeout = 500 # milliseconds
+    try:
+        client = MongoClient('localhost', 27017, serverSelectionTimeoutMS = timeout)
+        client.server_info() # Force connection check
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        traceback.print_exc()
+        print(err)
+        print("")
+        print("Check to see if mongodb is actually running. Exiting.")
+        print("")
+        sys.exit(-1)
+    return client
+
+def get_unique_name(client, db_base, nmax=100):
+    """Get a unique database name.
+    """
+    dbs = client.database_names()
+    for idx in range(0, nmax):
+        name_try = db_base + "_" + str(idx).zfill(2)
+        if not (name_try in dbs):
+            print("Using database name: %s" % name_try)
+            return name_try
+    print("Must drop or rename some databases.")
+    print("Maximum of %i databases named with %s are present." % (nmax,db_base))
+    print("Exiting.")
+    sys.exit(-1)
+    return None
+
+def import_initial_collections(db, cbasic):
+    """Import initial collections for use in creating specialized collections.
+    """
+    for cname in cbasic.keys():
+        print("Attempting import for %s" % cname)
+        fullpath = os.path.join(importpath, cbasic[cname])
+        istr = "mongoimport --file=%s --headerline --db=%s --collection=%s --type=csv" % (fullpath, db.name, cname)
+        iproc = subprocess.Popen(istr,shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE)
+        iproc.wait()
+        print(iproc.communicate())
+    print("Collections created:")
+    for cname in db.collection_names():
+        print(cname)
+    return
+
+def flag_data_to_be_ignored(db):
+    dred.main_exptivar(db, "ucsb_ivar_and_ivarplus")
+    dred.main_cdivar(db, "cd_ivar_2016")
+    dred.main_cdivar(db, "cd_ivar_2017")
+    dred.main_cdlwr(db, "cd_lwr_2017")
+    return
+
+if __name__ == "__main__":
+    client = get_mongo_client()
+    dbname = get_unique_name(client, db_base)
+    db = client[dbname]
+    import_initial_collections(db, cbasic)
+    flag_data_to_be_ignored(db)
+    sys.exit()
+sys.exit()
