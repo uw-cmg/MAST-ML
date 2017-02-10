b881b59acf350b2554942f5866006bbcc51e91ec
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-02-10 17:06:46 -0600

    Work with alloy names

--- LeaveOutAlloyCV.py
+++ LeaveOutAlloyCV.py
@@ -10,9 +10,19 @@ def execute(model, data, savepath, *args, **kwargs):
     rms_list = []
     alloy_list = []
     me_list = []
+   
+    #TTM modify alloys since they are no longer integers
+    alloys_nested = data.get_data("Alloy")
+    import itertools
+    chain = itertools.chain.from_iterable(alloys_nested)
+    alloys = list(chain)
+    alloys = list(set(alloys))
+    alloys.sort()
 
-    for alloy in range(1, max(data.get_data("Alloy"))[0] + 1):
-
+    print(alloys)
+    #for alloy in range(1, max(data.get_data("Alloy"))[0] + 1):
+    for alloy in alloys:
+        print(alloy)
         # fit model to all alloys except the one to be removed
         data.remove_all_filters()
         data.add_exclusive_filter("Alloy", '=', alloy)
@@ -33,16 +43,22 @@ def execute(model, data, savepath, *args, **kwargs):
 
     # graph rmse vs alloy 
     fig, ax = plt.subplots(figsize=(10, 4))
-    plt.xticks(np.arange(0, max(alloy_list) + 1, 5))
-    ax.scatter(alloy_list, rms_list, color='black', s=10)
-    ax.plot((0, max(data.get_data("Alloy"))[0]), (0, 0), ls="--", c=".3")
+    #TTM+4 alloy len
+    #plt.xticks(np.arange(0, max(alloy_list) + 1, 5))
+    plt.xticks(np.arange(0, len(alloys) + 1, 5))
+    ax.scatter(np.arange(0, len(alloy_list)), rms_list, color='black', s=10)
+    ax.set_xticklabels(alloy_list)
+    #ax.plot((0, max(data.get_data("Alloy"))[0]), (0, 0), ls="--", c=".3")
+    #TTM+1
+    ax.plot((0, len(alloys)), (0, 0), ls="--", c=".3")
     ax.set_xlabel('Alloy Number')
     ax.set_ylabel('RMSE (Mpa)')
     ax.set_title('Leave out Alloy')
     ax.text(.05, .88, 'Mean RMSE: {:.2f} MPa'.format(np.mean(rms_list)), fontsize=14, transform=ax.transAxes)
     ax.text(.05, .82, 'Mean Mean Error: {:.2f} MPa'.format(np.mean(me_list)), fontsize=14, transform=ax.transAxes)
     for x in np.argsort(rms_list)[-5:]:
-        ax.annotate(s = alloy_list[x],xy = (alloy_list[x], rms_list[x]))
+        #ax.annotate(s = alloy_list[x],xy = (alloy_list[x], rms_list[x]))
+        ax.annotate(s = alloy_list[x],xy = (x, rms_list[x]))
     fig.savefig(savepath.format(ax.get_title()), dpi=200, bbox_inches='tight')
     fig.clf()
     plt.close()
