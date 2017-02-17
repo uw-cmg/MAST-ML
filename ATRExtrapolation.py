9ffebbee077458eb655b5eeb8d9a5d83bde36b6f
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-02-17 13:11:24 -0600

    change field code

--- ATRExtrapolation.py
+++ ATRExtrapolation.py
@@ -4,13 +4,17 @@ from sklearn.metrics import mean_squared_error
 
 
 def execute(model, data, savepath, *args, **kwargs):
+    atr2data=2 #old
+    codestr = "Data Set code"
+    atr2data="ATR2"
+    codestr = "dataset"
     data.remove_all_filters()
-    data.add_exclusive_filter("Data Set code", '=', 2)
+    data.add_exclusive_filter(codestr, '=', atr2data)
     model.fit(data.get_x_data(), data.get_y_data())
 
     atr1_alloys = [6,34,35,36,37,38]
     data.remove_all_filters()
-    data.add_inclusive_filter("Data Set code", '=', 2)
+    data.add_inclusive_filter(codestr, '=', atr2data)
 
     Ypredict = model.predict(data.get_x_data())
     overall_rms = np.sqrt(mean_squared_error(data.get_y_data(), Ypredict))
@@ -20,7 +24,7 @@ def execute(model, data, savepath, *args, **kwargs):
     data.remove_all_filters()
     for x in atr1_alloys:
         data.add_inclusive_filter("Alloy", '=', x)
-    data.add_exclusive_filter("Data Set code", '<>', 2)
+    data.add_exclusive_filter(codestr, '<>', atr2data)
     Ypredict = model.predict(data.get_x_data())
     atr1_rms = np.sqrt(mean_squared_error(data.get_y_data(), Ypredict))
     plt.scatter(data.get_y_data(), Ypredict, lw = 0, color = '#03FF00', label = 'IVAR, ATR1, ATR2')
@@ -30,7 +34,7 @@ def execute(model, data, savepath, *args, **kwargs):
     data.add_inclusive_filter("Alloy",'<',60)
     for x in atr1_alloys:
         data.add_exclusive_filter("Alloy", '=', x)
-    data.add_exclusive_filter("Data Set code", '<>', 2)
+    data.add_exclusive_filter(codestr, '<>', atr2data)
     Ypredict = model.predict(data.get_x_data())
     ivar_rms = np.sqrt(mean_squared_error(data.get_y_data(), Ypredict))
     plt.scatter(data.get_y_data(), Ypredict, lw=0, color='#FF0034', label = 'IVAR, ATR2')
@@ -38,7 +42,7 @@ def execute(model, data, savepath, *args, **kwargs):
 
     data.remove_all_filters()
     data.add_inclusive_filter("Alloy", '>', 59)
-    data.add_exclusive_filter("Data Set code", '<>', 2)
+    data.add_exclusive_filter(codestr, '<>', atr2data)
     Ypredict = model.predict(data.get_x_data())
     atr2_rms = np.sqrt(mean_squared_error(data.get_y_data(), Ypredict))
     plt.scatter(data.get_y_data(), Ypredict, lw=0, color='#49CCFF', label = 'ATR2 only')
