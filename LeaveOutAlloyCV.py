d4229a4872fcf424af49d17b0f9978be9081c5c4
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-02-21 16:58:25 -0600

    make more generic

--- LeaveOutAlloyCV.py
+++ LeaveOutAlloyCV.py
@@ -15,6 +15,11 @@ def execute(model, data, savepath, *args, **kwargs):
         field_name = kwargs["field_name"]
     else:
         field_name = "No field set"
+        raise ValueError("field_name not in kwargs. %s" % field_name)
+    if "label_field_name" in kwargs.keys():
+        label_field_name = kwargs["label_field_name"]
+    else:
+        label_field_name = None
     if "xlabel" in kwargs.keys():
         xlabel = kwargs["xlabel"]
     else:
@@ -27,29 +32,39 @@ def execute(model, data, savepath, *args, **kwargs):
         title = kwargs["title"]
     else:
         title = ""
+
     rms_list = list()
     group_value_list = list()
+    group_label_list=list()
     me_list = list()
     group_list = list()
 
     Xdata = np.asarray(data.get_x_data())
     ydata = np.asarray(data.get_y_data()).ravel()
     fielddata = np.asarray(data.get_data(field_name)).ravel()
+    if not label_field_name == None:
+        labeldata = np.asarray(data.get_data(label_field_name)).ravel()
 
     indices = gttd.get_field_logo_indices(data, field_name)
+    
     groups = list(indices.keys())
     groups.sort()
     for group in groups:
         train_index = indices[group]["train_index"]
         test_index = indices[group]["test_index"]
         test_group_val = fielddata[test_index[0]] #left-out group value
+        if label_field_name == None:
+            test_group_label = "None"
+        else:
+            test_group_label = labeldata[test_index[0]]
         model.fit(Xdata[train_index], ydata[train_index])
-        Ypredict = model.predict(Xdata[test_index],ydata[test_index])
+        Ypredict = model.predict(Xdata[test_index])
 
-        rms = np.sqrt(mean_squared_error(Ypredict, np.array(data.get_y_data()).ravel()))
-        me = mean_error(Ypredict, np.array(data.get_y_data()).ravel())
+        rms = np.sqrt(mean_squared_error(Ypredict, ydata[test_index]))
+        me = mean_error(Ypredict, ydata[test_index])
         rms_list.append(rms)
         group_value_list.append(test_group_val)
+        group_label_list.append(test_group_label)
         me_list.append(me)
         group_list.append(group) #should be able to just use groups
 
@@ -58,24 +73,31 @@ def execute(model, data, savepath, *args, **kwargs):
 
     # graph rmse vs alloy 
     matplotlib.rcParams.update({'font.size': 18})
+    smallfont = 0.85*matplotlib.rcParams['font.size']
     fig, ax = plt.subplots(figsize=(10, 4))
-    plt.xticks(np.arange(0, max(group_list) + 1, 5)) #group_list is numeric
-    ax.scatter(group_list, rms_list, color='black', s=10)
+    plt.xticks(np.arange(0, max(group_value_list) + 1, 5))
+    ax.scatter(group_value_list, rms_list, color='black', s=10)
     #plot zero line
-    ax.plot((0, max(group_list)), (0, 0), ls="--", c=".3") 
+    ax.plot((0, max(group_value_list)+1), (0, 0), ls="--", c=".3") 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     if len(title) > 0:
         ax.set_title(title)
-    ax.text(.5, .90, 'Mean RMSE: {:.2f} MPa'.format(np.mean(rms_list)), transform=ax.transAxes, fontsize=0.85*matplotlib.rcParams['font.size'])
+    ax.text(.5, .90, 'Mean RMSE: {:.2f}'.format(np.mean(rms_list)), transform=ax.transAxes, fontsize=smallfont)
     for x in np.argsort(rms_list)[-5:]:
-        ax.annotate(s = alloy_list[x],xy = (alloy_list[x], rms_list[x]))
+        if label_field_name == None:
+            alabel = group_value_list[x]
+        else:
+            alabel = group_label_list[x]
+        ax.annotate(s = alabel,
+                    xy = (group_value_list[x], rms_list[x]),
+                    fontsize=smallfont)
     fig.savefig(test_name.replace(" ","_"), dpi=200, bbox_inches='tight')
     fig.clf()
     plt.close()
 
     csvname = "%s.csv" % test_name.replace(" ","_")
-    headerline = "Group,Group value,RMSE"
-    save_array = np.array([group_list, group_value_list, rms_list]).transpose()
-    ptools.array_to_csv(csvname, headerline, save_array, fmtstr=None) 
+    headerline = "Group,Group value,Group label,RMSE,Mean error"
+    save_array = np.asarray([group_list, group_value_list, group_label_list, rms_list, me_list]).transpose()
+    ptools.mixed_array_to_csv(csvname, headerline, save_array) 
     return
