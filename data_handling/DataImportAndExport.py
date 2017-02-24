f61cd668713c857160831fde7b6956efacee78de
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-02-24 12:17:20 -0600

    Increase atr2 standard time range

--- data_handling/DataImportAndExport.py
+++ data_handling/DataImportAndExport.py
@@ -336,7 +336,7 @@ def main(importpath):
     add_normalized_fields(db, "expt_atr2", ["expt_ivar","expt_atr2","cd1_lwr"])
     #
     create_standard_conditions(db, "lwr_std_expt",3e10,290,3e6,5e9,["expt_ivar","expt_atr2","cd1_lwr"])
-    create_standard_conditions(db, "atr2_std_expt",3.64e12,291,3e5,9e7,["expt_ivar","expt_atr2","cd1_lwr"])
+    create_standard_conditions(db, "atr2_std_expt",3.64e12,291,3e5,1.5e8,["expt_ivar","expt_atr2","cd1_lwr"])
     create_standard_conditions(db, "lwr_std_cd1",3e10,290,3e6,5e9,["cd1_ivar","cd1_lwr"])
     #
     cas.export_spreadsheet(db, "expt_ivar", exportpath)
