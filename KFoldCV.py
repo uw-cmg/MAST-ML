0184b5f50ffe8c4a39c7eadcab575b3ea0fdc081
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-03-13 12:23:12 -0500

    Generalize Kfold test

--- KFoldCV.py
+++ KFoldCV.py
@@ -1,82 +1,107 @@
+import os
 import matplotlib
 import numpy as np
 import data_parser
 import matplotlib.pyplot as plt
 from mean_error import mean_error
-from sklearn import cross_validation
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import LeavePOut
+from sklearn.model_selection import KFold
+from sklearn.model_selection import ShuffleSplit
 from sklearn.metrics import mean_squared_error
 import data_analysis.printout_tools as ptools
+import plot_data.plot_predicted_vs_measured as plotpm
 
-def execute(model, data, savepath, lwr_data="", *args, **kwargs):
-    if "num_runs" in kwargs.keys():
-        num_runs = int(kwargs["num_runs"])
-    else:
-        num_runs = 200
-    if "num_folds" in kwargs.keys():
-        num_folds = int(kwargs["num_folds"])
-    else:
-        num_runs = 5
-
+def execute(model, data, savepath, lwr_data="", 
+            num_runs=200,
+            num_folds=5,
+            xlabel="Measured",
+            ylabel="Predicted",
+            numericlabelfield=None,
+            *args, **kwargs):
+    """Basic cross validation
+        Args:
+            model, data, savepath, lwr_data: see AllTests.py
+            num_runs <int>: number of runs to repeat each cross-validation
+                            split (e.g. num_runs iterations of 5fold CV, where
+                            each 5-fold CV has 5 test-train sets)
+                            Default 200.
+            num_folds <int>: number of folds.
+            numericlabelfield <str>: Field name for numeric label
+    """
+    num_runs = int(num_runs)
+    num_folds = int(num_folds)
+    
     # get data
     Ydata = np.array(data.get_y_data()).ravel()
     Xdata = np.array(data.get_x_data())
+    dlen = len(Xdata)
+    indices = np.arange(0, dlen)
 
-    Y_predicted_best = []
-    Y_predicted_worst = []
-    Y_predicted_best_fold_numbers = []
-    Y_predicted_worst_fold_numbers = []
-    Worst_RMS=[]
-    Best_RMS=[]
+    print("Using %i %i-folds." % (num_runs,num_folds))
+    cvmodel = KFold(n_splits=num_folds, shuffle=True)
 
-    maxRMS = 1
-    minRMS = 100
+    Y_predicted_best = list()
+    Y_predicted_worst = list()
+    Y_predicted_best_fold_numbers = list()
+    Y_predicted_worst_fold_numbers = list()
 
-    RMS_List = []
-    ME_List = []
-    for n in range(num_runs):
-        kf = cross_validation.KFold(len(Xdata), n_folds=num_folds, shuffle=True)
-        K_fold_rms_list = []
-        K_fold_me_list = []
-        Overall_Y_Pred = np.zeros(len(Xdata))
-        Pred_Fold_Numbers = np.zeros(len(Xdata))
-        Overall_Abs_Err = np.zeros(len(Xdata))
+    maxRMS =  0
+    minRMS =  1000000
+
+    Mean_RMS_List = list()
+    Mean_ME_List = list()
+    for numrun in range(num_runs):
+        print("Run %i/%i" % (numrun+1, num_runs))
+        run_rms_list = list()
+        run_me_list = list()
+        Run_Y_Pred = np.empty(dlen)
+        Run_Fold_Numbers = np.empty(dlen)
+        Run_Abs_Err = np.empty(dlen)
+        Run_Y_Pred.fill(np.nan)
+        Run_Fold_Numbers.fill(np.nan)
+        Run_Abs_Err.fill(np.nan)
         # split into testing and training sets
-        nfold=0
-        for train_index, test_index in kf:
-            nfold = nfold + 1
-            X_train, X_test = Xdata[train_index], Xdata[test_index]
-            Y_train, Y_test = Ydata[train_index], Ydata[test_index]
+        ntest=0
+        for train, test in cvmodel.split(indices):
+            ntest = ntest + 1
+            print("Test %i.%i" % (numrun+1,ntest))
+            X_train, X_test = Xdata[train], Xdata[test]
+            Y_train, Y_test = Ydata[train], Ydata[test]
             # train on training sets
             model.fit(X_train, Y_train)
             Y_test_Pred = model.predict(X_test)
             rms = np.sqrt(mean_squared_error(Y_test, Y_test_Pred))
             me = mean_error(Y_test_Pred,Y_test)
-            K_fold_rms_list.append(rms)
-            K_fold_me_list.append(me)
-            Overall_Y_Pred[test_index] = Y_test_Pred
-            Pred_Fold_Numbers[test_index] = nfold
-            Overall_Abs_Err[test_index] = np.absolute(Y_test - Y_test_Pred)
+            run_rms_list.append(rms)
+            run_me_list.append(me)
+            Run_Y_Pred[test] = Y_test_Pred
+            Run_Fold_Numbers[test] = ntest
+            Run_Abs_Err[test] = np.absolute(Y_test - Y_test_Pred)
 
-        RMS_List.append(np.mean(K_fold_rms_list))
-        ME_List.append(np.mean(K_fold_me_list))
-        if np.mean(K_fold_rms_list) > maxRMS:
-            maxRMS = np.mean(K_fold_rms_list)
-            Y_predicted_worst = Overall_Y_Pred
-            Y_predicted_worst_fold_numbers = Pred_Fold_Numbers
-            Worst_Abs_Err = Overall_Abs_Err
+        mean_run_rms = np.mean(run_rms_list)
+        mean_run_me = np.mean(run_me_list)
+        Mean_RMS_List.append(mean_run_rms)
+        Mean_ME_List.append(mean_run_me)
+        if mean_run_rms > maxRMS:
+            maxRMS = mean_run_rms
+            Y_predicted_worst = Run_Y_Pred
+            Y_predicted_worst_fold_numbers = Run_Fold_Numbers
+            Worst_Abs_Err = Run_Abs_Err
 
-        if np.mean(K_fold_rms_list) < minRMS:
-            minRMS = np.mean(K_fold_rms_list)
-            Y_predicted_best = Overall_Y_Pred
-            Y_predicted_best_fold_numbers = Pred_Fold_Numbers
-            Best_Abs_Err = Overall_Abs_Err
+        if mean_run_rms < minRMS:
+            minRMS = mean_run_rms
+            Y_predicted_best = Run_Y_Pred
+            Y_predicted_best_fold_numbers = Run_Fold_Numbers
+            Best_Abs_Err = Run_Abs_Err
 
-    avgRMS = np.mean(RMS_List)
-    medRMS = np.median(RMS_List)
-    sd = np.std(RMS_List)
-    meanME = np.mean(ME_List)
+    avgRMS = np.mean(Mean_RMS_List)
+    medRMS = np.median(Mean_RMS_List)
+    sd = np.std(Mean_RMS_List)
+    meanME = np.mean(Mean_ME_List)
 
-    print("Using {}x {}-Fold CV: ".format(num_runs, num_folds))
+    if not (os.path.isdir(savepath)):
+        os.mkdir(savepath)
     print("The average RMSE was {:.3f}".format(avgRMS))
     print("The median RMSE was {:.3f}".format(medRMS))
     print("The max RMSE was {:.3f}".format(maxRMS))
@@ -84,36 +109,29 @@ def execute(model, data, savepath, lwr_data="", *args, **kwargs):
     print("The std deviation of the RMSE values was {:.3f}".format(sd))
     print("The average mean error was {:.3f}".format(meanME))
 
-    matplotlib.rcParams.update({'font.size': 18})
-    f, ax = plt.subplots(1, 2, figsize = (11,5))
-    ax[0].scatter(Ydata, Y_predicted_best, c='black', s=10)
-    ax[0].plot(ax[0].get_ylim(), ax[0].get_ylim(), ls="--", c=".3")
-    ax[0].set_title('Best Fit')
-    ax[0].text(.05, .88, 'Min RMSE: {:.2f}'.format(minRMS), transform=ax[0].transAxes)
-    ax[0].text(.05, .81, 'Mean RMSE: {:.2f}'.format(avgRMS), transform=ax[0].transAxes)
-    ax[0].text(.05, .74, 'Std. Dev.: {:.2f}'.format(sd), transform=ax[0].transAxes)
-    #ax[0].text(.05, .88, 'Min RMSE: {:.2f} MPa'.format(minRMS), transform=ax[0].transAxes)
-    #ax[0].text(.05, .81, 'Mean RMSE: {:.2f} MPa'.format(avgRMS), transform=ax[0].transAxes)
-    #ax[0].text(.05, .74, 'Std. Dev.: {:.2f} MPa'.format(sd), transform=ax[0].transAxes)
-    ax[0].set_xlabel('Measured (MPa)')
-    ax[0].set_ylabel('Predicted (MPa)')
+    notelist_best = list()
+    notelist_best.append("Min RMSE: {:.2f}".format(minRMS))
+    notelist_best.append("Mean RMSE: {:.2f}".format(avgRMS))
+    notelist_best.append("Std. Dev.: {:.2f}".format(sd))
+    
+    notelist_worst = list()
+    notelist_worst.append("Max RMSE.: {:.2f}".format(maxRMS))
+    kwargs=dict()
+    kwargs['xlabel'] = xlabel
+    kwargs['ylabel'] = ylabel
+    kwargs['notelist_best'] = notelist_best
+    kwargs['notelist_worst'] = notelist_worst
+    kwargs['savepath'] = savepath
 
-    ax[1].scatter(Ydata, Y_predicted_worst, c='black', s=10)
-    ax[1].plot(ax[1].get_ylim(), ax[1].get_ylim(), ls="--", c=".3")
-    ax[1].set_title('Worst Fit')
-    ax[1].text(.05, .88, 'Max RMSE: {:.2f}'.format(maxRMS), transform=ax[1].transAxes)
-    ax[1].set_xlabel('Measured (MPa)')
-    ax[1].set_ylabel('Predicted (MPa)')
+    plotpm.best_worst(Ydata, Y_predicted_best, Y_predicted_worst, **kwargs)
 
-    f.tight_layout()
-    f.savefig(savepath.format("cv_best_worst"), dpi=200, bbox_inches='tight')
-    plt.clf()
-    plt.close()
+    if numericlabelfield == None:
+        numericlabelfield = data.x_features[0]
 
-    alloys = np.asarray(data.get_data("alloy_number")).ravel()
-    csvname = "KFoldCV_data.csv"
-    headerline = "Alloy number,Measured,Predicted best,Absolute error best,Fold numbers best,Predicted worst,Absolute error worst,Fold numbers worst"
-    myarray = np.array([alloys, Ydata,
+    labels = np.asarray(data.get_data(numericlabelfield)).ravel()
+    csvname = os.path.join(savepath,"KFold_CV_data.csv")
+    headerline = "%s,Measured,Predicted best,Absolute error best,Fold numbers best,Predicted worst,Absolute error worst,Fold numbers worst" % numericlabelfield
+    myarray = np.array([labels, Ydata,
                 Y_predicted_best, Best_Abs_Err, 
                 Y_predicted_best_fold_numbers,
                 Y_predicted_worst,Worst_Abs_Err,
