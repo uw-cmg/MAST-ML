a2291e8ac1f6d4083673fb0770afc36e6931d11f
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-03-15 12:28:29 -0500

    Generalize leave out alloy to leave out group and add test

--- LeaveOutGroupCV.py
+++ LeaveOutGroupCV.py
@@ -6,6 +6,8 @@ import csv
 import matplotlib
 import portion_data.get_test_train_data as gttd
 import data_analysis.printout_tools as ptools
+import plot_data.plot_rmse as plotrmse
+
 def execute(model, data, savepath, 
             field_name=None,
             label_field_name=None,
@@ -54,30 +56,21 @@ def execute(model, data, savepath,
     print('Mean RMSE: ', np.mean(rms_list))
     print('Mean Mean Error: ', np.mean(me_list))
 
-    # graph rmse vs alloy 
-    matplotlib.rcParams.update({'font.size': 18})
-    smallfont = 0.85*matplotlib.rcParams['font.size']
-    fig, ax = plt.subplots(figsize=(10, 4))
-    plt.xticks(np.arange(0, max(group_value_list) + 1, 5))
-    ax.scatter(group_value_list, rms_list, color='black', s=10)
-    #plot zero line
-    ax.plot((0, max(group_value_list)+1), (0, 0), ls="--", c=".3") 
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    if len(title) > 0:
-        ax.set_title(title)
-    ax.text(.5, .90, 'Mean RMSE: {:.2f}'.format(np.mean(rms_list)), transform=ax.transAxes, fontsize=smallfont)
-    for x in np.argsort(rms_list)[-5:]:
-        if label_field_name == None:
-            alabel = group_value_list[x]
-        else:
-            alabel = group_label_list[x]
-        ax.annotate(s = alabel,
-                    xy = (group_value_list[x], rms_list[x]),
-                    fontsize=smallfont)
-    fig.savefig("leavegroupout_cv", dpi=200, bbox_inches='tight')
-    fig.clf()
-    plt.close()
+    notelist=list()
+    notelist.append("Mean RMSE: {:.2f}".format(np.mean(rms_list)))
+    
+    kwargs=dict()
+    kwargs['xlabel'] = xlabel
+    kwargs['ylabel'] = ylabel
+    kwargs['title'] = title
+    kwargs['notelist'] = notelist
+    kwargs['savepath'] = savepath
+    if label_field_name == None:
+        kwargs['group_label_list'] = None
+    else:
+        kwargs['group_label_list'] = group_label_list
+
+    plotrmse.vs_leftoutgroup(group_value_list, rms_list, **kwargs)
 
     csvname = "leavegroupout.csv"
     headerline = "Group,Group value,Group label,RMSE,Mean error"
