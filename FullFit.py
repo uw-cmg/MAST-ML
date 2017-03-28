09104a72040fb54c5be27a4c14c7dae73896e278
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-03-28 14:38:33 -0500

    Remove old code

--- FullFit.py
+++ FullFit.py
@@ -62,39 +62,6 @@ def execute(model, data, savepath,
     kwargs['savepath'] = savepath
     kwargs['guideline'] = 1
     [ypredict, y_abs_err, rmse, mean_error] = do_single_fit(model, Xdata, ydata, **kwargs)
-    #datasets = ['IVAR', 'ATR-1', 'ATR-2']
-    #colors = ['#BCBDBD', '#009AFF', '#FF0A09']
-    #fig, ax = plt.subplots()
-
-    #calculate rms for each dataset
-    #TTM want data to be separated out earlier. Just calculate RMS.
-    #for dataset in range(max(np.asarray(data.get_data("Data Set code")).ravel()) + 1):
-    #    data.remove_all_filters()
-    #    data.add_inclusive_filter("Data Set code", '=', dataset)
-    #    Ypredict = model.predict(data.get_x_data())
-    #    Ydata = np.asarray(data.get_y_data()).ravel()
-    #    # calculate rms
-    #    rms = np.sqrt(mean_squared_error(Ypredict, Ydata))
-    #    # graph outputs
-    #    ax.scatter(Ydata, Ypredict, s=7, color=colors[dataset], label= datasets[dataset], lw = 0)
-    #    #ax.text(.05, .83 - .05*dataset, '{} RMS: {:.3f}'.format(datasets[dataset],rms), fontsize=14, transform=ax.transAxes)
-    # calculate rms
-    #rms = np.sqrt(mean_squared_error(Ypredict, Ydata))
-    # graph outputs
-    #dataset=0 #TTM dummy index value
-    #matplotlib.rcParams.update({'font.size': 18})
-    #ax.scatter(Ydata, Ypredict, s=7, color=colors[dataset], label= datasets[dataset], lw = 0)
-    #ax.text(.05, .83 - .05*dataset, '{} RMS: {:.3f}'.format(datasets[dataset],rms), transform=ax.transAxes)
-    #
-    #ax.legend()
-    #ax.plot(ax.get_ylim(), ax.get_ylim(), ls="--", c=".3")
-    #ax.set_xlabel('Measured (MPa)')
-    #ax.set_ylabel('Predicted (MPa)')
-    #ax.set_title('Full Fit')
-    #ax.text(.05, .88, 'Overall RMS: %.4f' % (overall_rms), transform=ax.transAxes)
-    #fig.savefig(savepath.format(ax.get_title()), dpi=300, bbox_inches='tight')
-    #plt.clf()
-    #plt.close()
 
     if numeric_field_name == None: #help identify each point
         numeric_field_name = data.x_features[0]
