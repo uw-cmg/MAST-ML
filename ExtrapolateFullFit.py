754bfa3e76c9514964c7c284754516244b269cb5
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-04-25 12:20:18 -0500

    Use dictionary for test data to allow multiple series

--- ExtrapolateFullFit.py
+++ ExtrapolateFullFit.py
@@ -11,8 +11,7 @@ import FullFit
 import plot_data.plot_xy as plotxy
 
 def execute(model, data, savepath, 
-        test_csv = None,
-        standard_csv = None,
+        test_csvs = None,
         group_field_name = None,
         label_field_name = None,
         numeric_field_name = None,
@@ -21,13 +20,17 @@ def execute(model, data, savepath,
         split_xlabel = "X",
         split_ylabel = "Prediction",
         stepsize=1,
+        markers="",
+        outlines="",
+        labels="",
+        sizes="",
+        manypoints_to_lines=50,
         measured_error_field_name = None,
         plot_filter_out = "",
         fit_only_on_matched_groups = 0,
         *args,**kwargs):
     """Do full fit for extrapolation.
-        test_csv <str>: test csv name
-        standard_csv <str>: standard csv name
+        test_csvs <str>: comma-delimited list of test csv name
         group_field_name <str>: field name for grouping data
         label_field_name <str>: field name for labels corresponding to groups
         numeric_field_name <str>: field name for numeric data that will be used
@@ -50,77 +53,46 @@ def execute(model, data, savepath,
                                           0 - fit on all data in the training
                                                 dataset (default)
                                         Only works if group_field_name is set.
+        markers <str>: comma-delimited marker list for split plots
+        outlines <str>: comma-delimited color list for split plots
+        labels <str>: comma-delimited list of labels for split plots; order
+                        should be Train, Test, Test, Test... with testing
+                        data in order of test_csv
+        manypoints_to_lines <int>: number of points after which a line should
+                    be used instead of markers
     """
+    manypoints_to_lines = int(manypoints_to_lines)
     stepsize=float(stepsize)
     #get datasets
     ##training
     train_xdata = np.asarray(data.get_x_data())
     train_ydata = np.asarray(data.get_y_data()).ravel()
-    ##testing
-    if test_csv == None:
-        raise ValueError("test_csv is None")
-    test_data = data_parser.parse(test_csv)
-    if len(plot_filter_out) > 0:
-        ftriplets = plot_filter_out.split(";")
-        for ftriplet in ftriplets:
-            fpcs = ftriplet.split(",")
-            ffield = fpcs[0].strip()
-            foperator = fpcs[1].strip()
-            fval = fpcs[2].strip()
-            try:
-                fval = float(fval)
-            except (ValueError, TypeError):
-                pass
-            test_data.add_exclusive_filter(ffield, foperator, fval)
-    test_data.set_y_feature(data.y_feature) # same feature as fitting data
-    test_data.set_x_features(data.x_features) # same features as fitting data
-    test_xdata = np.asarray(test_data.get_x_data())
-    test_ydata = np.asarray(test_data.get_y_data()).ravel()
     if numeric_field_name == None: #help identify each point
         numeric_field_name = data.x_features[0]
-    # filter out for only matched groups
-    if int(fit_only_on_matched_groups) == 1:
-        if group_field_name == None:
-            pass
-        else:
-            train_groupdata = np.asarray(data.get_data(group_field_name)).ravel()
-            test_groupdata = np.asarray(test_data.get_data(group_field_name)).ravel()
-            train_indices = gttd.get_logo_indices(train_groupdata)
-            groups_not_in_test = np.setdiff1d(train_groupdata, test_groupdata)
-            for outgroup in groups_not_in_test:
-                data.add_exclusive_filter(group_field_name,"=",outgroup)
-            train_xdata = np.asarray(data.get_x_data())
-            train_ydata = np.asarray(data.get_y_data()).ravel()
-    #
-    kwargs_f = dict()
-    kwargs_f['xlabel'] = xlabel
-    kwargs_f['ylabel'] = ylabel
-    kwargs_f['stepsize'] = stepsize
-    kwargs_f['numeric_field_name'] = numeric_field_name
-    kwargs_f['group_field_name'] = group_field_name
-    kwargs_f['savepath'] = os.path.join(savepath,'test')
-    if not (os.path.isdir(kwargs_f['savepath'])):
-        os.mkdir(kwargs_f['savepath'])
-    kwargs_f['do_pergroup_fits'] = 0 # NO per-group fitting
-    kwargs_f['test_numericdata'] = np.asarray(test_data.get_data(numeric_field_name)).ravel()
-    if not(group_field_name == None):
-        kwargs_f['test_groupdata'] = np.asarray(test_data.get_data(group_field_name)).ravel()
-        kwargs_f['train_groupdata'] = np.asarray(data.get_data(group_field_name)).ravel()
-        if label_field_name == None:
+    if not(group_field_name is None):
+        if label_field_name is None:
             label_field_name = group_field_name
-        kwargs_f['label_field_name'] = label_field_name
-        kwargs_f['test_labeldata'] = np.asarray(test_data.get_data(label_field_name)).ravel()
-    if not(measured_error_field_name == None):
-        kwargs_f['test_yerrdata'] = np.asarray(test_data.get_data(measured_error_field_name)).ravel()
-    kwargs_f['full_xtrain'] = train_xdata
-    kwargs_f['full_ytrain'] = train_ydata
-    kwargs_f['full_xtest'] = test_xdata
-    kwargs_f['full_ytest'] = test_ydata
-    test_data_array = FullFit.do_full_fit(model, **kwargs_f)
-    
-    ##standard has no y
-    if not(standard_csv == None):
-        std_data = data_parser.parse(standard_csv)
+    ##testing
+    lsplit = labels.split(",")
+    data_labels = list()
+    test_labels = list()
+    for litem in lsplit:
+        data_labels.append(litem.strip())
+    test_labels = list(data_labels[1:]) #not the training label
+    if test_csvs == None:
+        raise ValueError("test_csvs is None")
+    test_csv_list = list()
+    tcsplit = test_csvs.split(",")
+    for tcitem in tcsplit:
+        test_csv_list.append(tcitem.strip())
+    test_sets = dict()
+    for tidx in range(0,len(test_labels)):
+        test_csv = test_csv_list[tidx]
+        test_label = test_labels[tidx]
+        test_sets[test_label]=dict()
+        unfiltered_test_data = data_parser.parse(test_csv)
+        test_sets[test_label]['test_data_unfiltered'] = unfiltered_test_data
+        test_data = data_parser.parse(test_csv)
         if len(plot_filter_out) > 0:
             ftriplets = plot_filter_out.split(";")
             for ftriplet in ftriplets:
@@ -132,41 +104,76 @@ def execute(model, data, savepath,
                     fval = float(fval)
                 except (ValueError, TypeError):
                     pass
-                std_data.add_exclusive_filter(ffield, foperator, fval)
-        std_data.set_y_feature(data.x_features[0]) # dummy y feature
-        std_data.set_x_features(data.x_features) # same features as fitting data
-        std_xdata = np.asarray(std_data.get_x_data())
-        std_ydata = np.asarray(std_data.get_y_data()).ravel()
-        kwargs_s = dict()
-        kwargs_s['xlabel'] = xlabel
-        kwargs_s['ylabel'] = ylabel
-        kwargs_s['stepsize'] = stepsize
-        kwargs_s['numeric_field_name'] = numeric_field_name
-        kwargs_s['group_field_name'] = group_field_name
-        kwargs_s['savepath'] = savepath
-        kwargs_s['savepath'] = os.path.join(savepath,'standard')
-        if not (os.path.isdir(kwargs_s['savepath'])):
-            os.mkdir(kwargs_s['savepath'])
-        kwargs_s['fitlabel'] = "standard"
-        kwargs_s['do_pergroup_fits'] = 0 # NO per-group fitting
-        kwargs_s['test_numericdata'] = np.asarray(std_data.get_data(numeric_field_name)).ravel()
+                test_data.add_exclusive_filter(ffield, foperator, fval)
+        hasy = test_data.set_y_feature(data.y_feature) # same feature as fitting data
+        if hasy:
+            test_sets[test_label]['test_ydata'] = np.asarray(test_data.get_y_data()).ravel()
+        else:
+            test_sets[test_label]['test_ydata'] = None
+        test_data.set_x_features(data.x_features) #same features as fitting data
+        test_sets[test_label]['test_data']=test_data
+        test_sets[test_label]['test_xdata'] = np.asarray(test_data.get_x_data())
+        test_sets[test_label]['test_numericdata'] = np.asarray(test_data.get_data(numeric_field_name)).ravel()
+        if not(group_field_name is None):
+            test_sets[test_label]['test_groupdata'] = np.asarray(test_data.get_data(group_field_name)).ravel()
+            test_sets[test_label]['test_labeldata'] = np.asarray(test_data.get_data(label_field_name)).ravel()
+        else:
+            test_sets[test_label]['test_groupdata'] = None
+            test_sets[test_label]['test_labeldata'] = None
+        if not(measured_error_field_name == None):
+            test_sets[test_label]['test_yerrdata'] = np.asarray(test_data.get_data(measured_error_field_name)).ravel()
+        else:
+            test_sets[test_label]['test_yerrdata'] = None
+    # filter out for only matched groups
+    if int(fit_only_on_matched_groups) == 1:
+        if group_field_name == None:
+            pass
+        else:
+            train_groupdata = np.asarray(data.get_data(group_field_name)).ravel()
+            train_indices = gttd.get_logo_indices(train_groupdata)
+            for test_label in test_labels:
+                test_groupdata = test_sets[test_label]['test_groupdata']
+                groups_not_in_test = np.setdiff1d(train_groupdata, test_groupdata)
+                for outgroup in groups_not_in_test:
+                    data.add_exclusive_filter(group_field_name,"=",outgroup)
+                train_xdata = np.asarray(data.get_x_data())
+                test_sets[test_label]['train_xdata'] = train_xdata
+                train_ydata = np.asarray(data.get_y_data()).ravel()
+                test_sets[test_label]['train_ydata'] = train_ydata
+                train_groupdata = np.asarray(data.get_data(group_field_name)).ravel()
+                test_sets[test_label]['train_groupdata'] = train_groupdata
+                data.remove_all_filters()
+    #
+    for test_label in test_labels:
+        kwargs_f = dict()
+        kwargs_f['xlabel'] = xlabel
+        kwargs_f['ylabel'] = ylabel
+        kwargs_f['stepsize'] = stepsize
+        kwargs_f['numeric_field_name'] = numeric_field_name
+        kwargs_f['group_field_name'] = group_field_name
+        kwargs_f['savepath'] =os.path.join(savepath,test_label.replace(" ","_"))
+        if not (os.path.isdir(kwargs_f['savepath'])):
+            os.mkdir(kwargs_f['savepath'])
+        kwargs_f['do_pergroup_fits'] = 0 # NO per-group fitting
+        kwargs_f['test_numericdata'] = test_sets[test_label]['test_numericdata']
         if not(group_field_name == None):
-            kwargs_s['test_groupdata'] = np.asarray(std_data.get_data(group_field_name)).ravel()
-            kwargs_s['train_groupdata'] = np.asarray(std_data.get_data(group_field_name)).ravel()
-            if label_field_name == None:
-                label_field_name = group_field_name
-            kwargs_s['label_field_name'] = label_field_name
-            kwargs_s['test_labeldata'] = np.asarray(std_data.get_data(label_field_name)).ravel()
-        kwargs_s['full_xtrain'] = train_xdata
-        kwargs_s['full_ytrain'] = train_ydata
-        kwargs_s['full_xtest'] = std_xdata
-        kwargs_s['full_ytest'] = std_ydata
-        std_data_array = FullFit.do_full_fit(model, **kwargs_s)
-    else:
-        std_data_array = None
-    if numeric_field_name is None:
+            kwargs_f['test_groupdata'] = test_sets[test_label]['test_groupdata']
+            kwargs_f['train_groupdata'] = test_sets[test_label]['train_groupdata']
+            kwargs_f['label_field_name'] = label_field_name
+            kwargs_f['test_labeldata'] = test_sets[test_label]['test_labeldata']
+        if not(measured_error_field_name == None):
+            kwargs_f['test_yerrdata'] = test_sets[test_label]['test_yerrdata']
+        kwargs_f['full_xtrain'] = test_sets[test_label]['train_xdata']
+        kwargs_f['full_ytrain'] = test_sets[test_label]['train_ydata']
+        kwargs_f['full_xtest'] = test_sets[test_label]['test_xdata']
+        kwargs_f['full_ytest'] = test_sets[test_label]['test_ydata']
+        test_data_array = FullFit.do_full_fit(model, **kwargs_f)
+        test_sets[test_label]['fullfit_array'] = test_data_array
+    
+    if numeric_field_name is None: #no numeric plots to do
         return
     else:
+        return #TTM DEBUG
         if group_field_name == None:
             train_data_array = np.array([np.asarray(data.get_data(numeric_field_name)).ravel(), train_ydata]).transpose()
         else:
