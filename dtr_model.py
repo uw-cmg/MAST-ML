8948055ee90cd343cb1eee56122ffaeba6784e13
rjacobs914 <rjacobs3@wisc.edu>
2017-04-28 16:54:01 -0500

    added deprecation warning

--- dtr_model.py
+++ dtr_model.py
@@ -2,10 +2,12 @@ import configuration_parser
 import sklearn.tree as tree
 
 def get():
-	config = configuration_parser.parse()
-	exec('max_depth=' + config.get(__name__, 'max_depth'),locals(),globals())
-	min_samples_split = config.getint(__name__, 'min_samples_split')
-	min_samples_leaf = config.getint(__name__, 'min_samples_leaf')
-	criterion = config.get(__name__, 'split criterion')
-	return tree.DecisionTreeRegressor(criterion=criterion,max_depth=max_depth,min_samples_leaf=min_samples_leaf,
+    from warnings import warn
+    warn('Usage of dtr_model.py will be deprecated', DeprecationWarning)
+    config = configuration_parser.parse()
+    exec('max_depth=' + config.get(__name__, 'max_depth'),locals(),globals())
+    min_samples_split = config.getint(__name__, 'min_samples_split')
+    min_samples_leaf = config.getint(__name__, 'min_samples_leaf')
+    criterion = config.get(__name__, 'split criterion')
+    return tree.DecisionTreeRegressor(criterion=criterion,max_depth=max_depth,min_samples_leaf=min_samples_leaf,
 	                                  min_samples_split=min_samples_split)
