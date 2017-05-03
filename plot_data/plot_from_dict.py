e26513ef9563520e0df9ed3cc60681fe323bbade
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-05-03 13:09:08 -0500

    Only print rmse if exists

--- plot_data/plot_from_dict.py
+++ plot_data/plot_from_dict.py
@@ -11,6 +11,9 @@ import plot_data.plot_xy as plotxy
 
 def plot_group_splits_with_outliers(group_dict=None, outlying_groups=list(), label="group_splits", group_notelist=list(), addl_kwargs=dict(),
     *args, **kwargs):
+    """Plot from a data dictionary.
+        Converts into plot_xy.multiple_overlay
+    """
     xdatalist=list()
     ydatalist=list()
     labellist=list()
@@ -21,15 +24,18 @@ def plot_group_splits_with_outliers(group_dict=None, outlying_groups=list(), lab
     otherydata=list()
     groups = list(group_dict.keys())
     groups.sort()
+    show_rmse = 0
     for group in groups:
         if group in outlying_groups:
-            rmse = group_dict[group]['rmse']
             xdatalist.append(group_dict[group]['xdata'])
             xerrlist.append(group_dict[group]['xerrdata'])
             ydatalist.append(group_dict[group]['ydata'])
             yerrlist.append(None)
             labellist.append(group)
-            group_notelist.append('{:<1}: {:.2f}'.format(group, rmse))
+            if 'rmse' in group_dict[group].keys():
+                show_rmse = 1 # if any RMSE shown, do RMSE for remaining
+                rmse = group_dict[group]['rmse']
+                group_notelist.append('{:<1}: {:.2f}'.format(group, rmse))
         else:
             otherxdata.extend(group_dict[group]['xdata'])
             otherxerrdata.extend(group_dict[group]['xerrdata'])
@@ -40,8 +46,9 @@ def plot_group_splits_with_outliers(group_dict=None, outlying_groups=list(), lab
         ydatalist.insert(0,otherydata)
         yerrlist.insert(0,None)
         labellist.insert(0,"All others")
-        all_other_rmse = np.sqrt(mean_squared_error(otherydata, otherxdata))
-        group_notelist.append('{:<1}: {:.2f}'.format("All others", all_other_rmse))
+        if show_rmse == 1:
+            all_other_rmse = np.sqrt(mean_squared_error(otherydata, otherxdata))
+            group_notelist.append('{:<1}: {:.2f}'.format("All others", all_other_rmse))
     kwargs=dict()
     kwargs['xlabel'] = "X"
     kwargs['ylabel'] = "Y"
