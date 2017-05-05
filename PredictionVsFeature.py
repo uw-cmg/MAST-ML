0677e8c9cd316efe14ba430c906b78dda2a54453
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-05-05 13:41:44 -0500

    Add feature plots

--- PredictionVsFeature.py
+++ PredictionVsFeature.py
@@ -9,13 +9,12 @@ import data_analysis.printout_tools as ptools
 import portion_data.get_test_train_data as gttd
 from SingleFit import timeit
 from SingleFit import SingleFit
-from SingleFitGrouped import SingleFitGrouped
 import plot_data.plot_xy as plotxy
 import plot_data.plot_from_dict as plotdict
 import copy
 import time
 
-class PredictionVsFeature(SingleFitGrouped):
+class PredictionVsFeature(SingleFit):
     """Make prediction vs. feature plots from a single fit.
         training_dataset,
         testing_dataset, (Multiple testing datasets are allowed as a list.)
@@ -28,10 +27,8 @@ class PredictionVsFeature(SingleFitGrouped):
         xlabel,
         ylabel,
         stepsize,
-        plot_filter_out,
-        grouping_feature,
-        fit_only_on_matched_groups,
-        mark_outlying_groups, see parent class.
+        plot_filter_out, see parent class.
+        grouping_feature <str>: feature for grouping (optional)
         feature_plot_xlabel <str>: x-axis label for per-group plots of predicted and
                                 measured y data versus data from field of
                                 numeric_field_name
@@ -40,9 +37,7 @@ class PredictionVsFeature(SingleFitGrouped):
         markers <str>: comma-delimited marker list for split plots
         outlines <str>: comma-delimited color list for split plots
         linestyles <str>: comma-delimited list of line styles for split plots
-        data_labels <str>: comma-delimited list of labels for split plots; order
-                        should be Train, Test, Test, Test... with testing
-                        data in order of test_csv
+        data_labels <str>: comma-delimited list of testing dataset labels for split plots
     """
     def __init__(self, 
         training_dataset=None,
@@ -58,8 +53,6 @@ class PredictionVsFeature(SingleFitGrouped):
         stepsize=1,
         plot_filter_out = "",
         grouping_feature = None,
-        mark_outlying_groups = 2,
-        fit_only_on_matched_groups = 0,
         feature_plot_xlabel = "X",
         feature_plot_ylabel = "Prediction",
         feature_plot_feature = "",
@@ -72,6 +65,7 @@ class PredictionVsFeature(SingleFitGrouped):
             Additional class attributes not in parent class:
            
             Set by keyword:
+            self.grouping_feature <str>: feature for grouping data (optional)
             self.testing_datasets <list of data objects>: testing datasets
             self.feature_plot_xlabel <str>: x-axis label for feature plots
             self.feature_plot_ylabel <str>: y-axis label for feature plots
@@ -82,10 +76,13 @@ class PredictionVsFeature(SingleFitGrouped):
             self.data_labels <list of str>: list of data labels for plotting
             
             Set by code:
-            self.testing_dataset_dict <dict of data objects>: testing datasets
-            self.sfg_dict <dict of SingleFitGrouped objects>
+            self.testing_dataset_dict <dict of data objects>: testing datasets and their information:
+                dataset
+                feature_data
+                group_indices (if self.grouping_feature is set)
+                SingleFit (SingleFit object)
         """
-        SingleFitGrouped.__init__(self, 
+        SingleFit.__init__(self, 
             training_dataset=training_dataset, 
             testing_dataset=testing_dataset,
             model=model, 
@@ -97,11 +94,9 @@ class PredictionVsFeature(SingleFitGrouped):
             xlabel=xlabel,
             ylabel=ylabel,
             stepsize=stepsize,
-            plot_filter_out = plot_filter_out,
-            grouping_feature = grouping_feature,
-            mark_outlying_groups = mark_outlying_groups,
-            fit_only_on_matched_groups = fit_only_on_matched_groups)
+            plot_filter_out = plot_filter_out)
         #Sets by keyword
+        self.grouping_feature = grouping_feature
         self.testing_datasets = testing_dataset #expect a list of one or more testing datasets
         self.feature_plot_xlabel = feature_plot_xlabel
         self.feature_plot_ylabel = feature_plot_ylabel
@@ -112,40 +107,38 @@ class PredictionVsFeature(SingleFitGrouped):
         self.linestyles = linestyles
         #Sets in code
         self.testing_dataset_dict = dict() 
-        self.sfg_dict=dict()
         return
    
     @timeit
-    def run(self):
-        self.set_up()
-        self.get_sfg_dict()
-        self.plot()
-        self.print_readme()
-        return
-
-    @timeit
     def set_up(self):
-        self.readme_list.append("%s\n" % time.asctime())
+        SingleFit.set_up(self)
         tidxs = np.arange(0, len(self.testing_datasets)) 
         for tidx in tidxs:
-            test_data_label = self.data_labels[tidx+1] #first one is training
-            self.testing_dataset_dict[test_data_label] = copy.deepcopy(self.testing_datasets[tidx])
+            td_entry = dict()
+            td_entry['dataset'] = copy.deepcopy(self.testing_datasets[tidx])
+            td_entry['feature_data'] = np.asarray(td_entry['dataset'].get_data(self.feature_plot_feature)).ravel()
+            if not (self.grouping_feature is None):
+                td_group_data = np.asarray(td_entry['dataset'].get_data(self.grouping_feature)).ravel()
+                td_entry['group_indices'] = gttd.get_logo_indices(td_group_data)
+            test_data_label = self.data_labels[tidx]
+            self.testing_dataset_dict[test_data_label] = dict(td_entry)
         return
 
     @timeit
-    def get_sfg_dict(self):
-        """Get SingleFitGrouped dictionary.
-        
-            Each entry will be a SingleFitGrouped object,
-            one for each of the testing datasets.
+    def predict(self):
+        self.get_singlefits()
+        return
+
+    def get_singlefits(self):
+        """Get SingleFit for each testing dataset.
         """
         self.readme_list.append("----- Fitting and prediction -----\n")
         self.readme_list.append("See results per dataset in subfolders\n")
         for testset in self.testing_dataset_dict.keys():
             self.readme_list.append("  %s\n" % testset)
-            sfg_obj = SingleFitGrouped( 
+            self.testing_dataset_dict[testset]['SingleFit'] = SingleFit( 
                 training_dataset = self.training_dataset, 
-                testing_dataset = self.testing_dataset_dict[testset],
+                testing_dataset = self.testing_dataset_dict[testset]['dataset'],
                 model = self.model, 
                 save_path = os.path.join(self.save_path, str(testset)),
                 input_features = self.input_features, 
@@ -155,175 +148,83 @@ class PredictionVsFeature(SingleFitGrouped):
                 xlabel=self.xlabel,
                 ylabel=self.ylabel,
                 stepsize=self.stepsize,
-                plot_filter_out = self.plot_filter_out,
-                grouping_feature = self.grouping_feature,
-                mark_outlying_groups = self.mark_outlying_groups,
-                fit_only_on_matched_groups = self.fit_only_on_matched_groups)
-            # Not all testing sets will have target data
-            sfg_obj.set_group_info()
-            if sfg_obj.fit_only_on_matched_groups == 1:
-                out_training_groups = np.setdiff1d(sfg_obj.train_groups, 
-                                                    sfg_obj.test_groups)
-                for group in out_training_groups:
-                    sfg_obj.training_dataset.add_exclusive_filter(sfg_obj.grouping_feature,"=",group)
-            SingleFit.set_data(sfg_obj)
-            sfg_obj.fit()
-            sfg_obj.get_prediction()
-            sfg_obj.print_output_csv()
-            if not (sfg_obj.testing_target_data is None):
-                sfg_obj.get_statistics()
-                sfg_obj.print_statistics()
-            sfg_obj.print_readme()
-            self.sfg_dict[testset] = sfg_obj
+                plot_filter_out = self.plot_filter_out)
+            self.testing_dataset_dict[testset]['SingleFit'].run()
         return
 
     @timeit
     def plot(self):
-        return
-
-    @timeit
-    def make_overall_plot(self, plabel="unfiltered_overall", use_filters=False):
-        """Make overall plot of predicted vs. measured"""
-        edict=dict() #here 'groups' will be data series. 
-        group_notelist = list()
-        if use_filters:
-            group_notelist.append("Data not displayed:")
-            for pfstr in self.plot_filter_out:
-                group_notelist.append(pfstr.replace(";"," "))
-            group_notelist.append("RMSEs for displayed data:")
-        else:
-            group_notelist.append("RMSEs:")
-        for label in self.extrapolation_dict.keys():
-            if not('rmse' in self.extrapolation_dict[label].overall_analysis.statistics.keys()):
-                continue #no measured data; cannot be plotted
-            s_analysis = self.extrapolation_dict[label].overall_analysis
-            if use_filters:
-                use_index = self.plot_filter_dict[label]['nogroup']
-            else:
-                use_index = np.arange(0, len(s_analysis.testing_target_prediction))
-            edict[label] = dict()
-            edict[label]['xdata'] = s_analysis.testing_target_data[use_index]
-            if self.target_error_feature is None:
-                edict[label]['xerrdata'] = np.zeros(len(use_index))
-            else:
-                edict[label]['xerrdata'] = s_analysis.testing_target_data_error[use_index]
-            edict[label]['ydata'] = s_analysis.testing_target_prediction[use_index]
-            edict[label]['rmse'] = np.sqrt(mean_squared_error(edict[label]['ydata'],edict[label]['xdata']))
-        series_list = list(edict.keys())
-        if len(series_list) == 0:
-            logging.info("No series for overall plot in extrapolatefullfit.")
+        self.readme_list.append("----- Plotting -----\n")
+        self.readme_list.append("See plots in subfolders:\n")
+        self.make_series_feature_plot(group=None)
+        if self.grouping_feature is None:
+            self.readme_list.append("Grouping feature is not set. No group plots to do.\n")
             return
-        addl_kwargs=dict()
-        addl_kwargs['save_path'] = os.path.join(self.save_path, plabel)
-        plotdict.plot_group_splits_with_outliers(group_dict=edict,
-            outlying_groups = series_list,
-            label=plabel,
-            group_notelist = list(group_notelist),
-            addl_kwargs = dict(addl_kwargs))
-        return
-    
-    @timeit
-    def make_plotting_filter_index(self, dataset, preexisting_index=None):
-        plot_index = list()
-        out_index = list()
-        for pfstr in self.plot_filter_out:
-            pflist = pfstr.split(";")
-            feature = pflist[0].strip()
-            symbol = pflist[1].strip()
-            rawvalue = pflist[2].strip()
-            try:
-                value = float(rawvalue)
-            except ValueError:
-                value = rawvalue
-            featuredata = np.asarray(dataset.get_data(feature)).ravel()
-            for fidx in range(0, len(featuredata)):
-                fdata = featuredata[fidx]
-                if fdata is None:
-                    continue
-                if symbol == "<":
-                    if fdata < value:
-                        out_index.append(fidx)
-                elif symbol == ">":
-                    if fdata > value:
-                        out_index.append(fidx)
-                elif symbol == "=":
-                    if fdata == value:
-                        out_index.append(fidx)
-        if preexisting_index is None:
-            preexisting_index = np.arange(0, len(np.asarray(dataset.get_data(feature)).ravel()))
-        out_index = np.unique(out_index)
-        plot_index = np.setdiff1d(preexisting_index, out_index)
-        return plot_index
-
-    @timeit
-    def make_plotting_filter_dict(self):
-        """Needs rework with dataframes
-        """
-        pdict=dict()
-        for label in self.extrapolation_dict.keys():
-            pdict[label]=dict()
-            pdict[label]['nogroup'] = self.make_plotting_filter_index(self.extrapolation_dict[label].testing_dataset)
-            if self.group_field_name is None:
-                pass
-            else:
-                for group in self.extrapolation_dict[label].test_groups:
-                    pdict[label][group] = self.make_plotting_filter_index(self.extrapolation_dict[label].testing_dataset, self.extrapolation_dict[label].test_group_indices[group]['test_index'])
-        #for training data, all have same training data; use last label
-        tlabel = self.data_labels[0]
-        pdict[tlabel]=dict()
-        pdict[tlabel]['nogroup'] = self.make_plotting_filter_index(self.training_dataset[0])
-        if self.group_field_name is None:
-            pass
-        else:
-            for group in self.extrapolation_dict[label].train_groups:
-                pdict[tlabel][group] = self.make_plotting_filter_index(self.training_dataset[0], self.extrapolation_dict[label].train_group_indices[group]['test_index'])
-        self.plot_filter_dict = dict(pdict)
+        allgroups=list()
+        for testset in self.testing_dataset_dict.keys():
+            for group in self.testing_dataset_dict[testset]['group_indices'].keys():
+                allgroups.append(group)
+        allgroups = np.unique(allgroups)
+        for group in allgroups:
+            self.make_series_feature_plot(group=group)
         return
 
     @timeit
-    def make_series_feature_plot(self, plabel="feature_plots", use_filters=False, show_training=True, group=None):
+    def make_series_feature_plot(self, group=None):
         """Make series feature plot
         """
-        edict=dict() #here 'groups' will be data series. 
-        group_notelist = list()
-        if use_filters:
+        plabel = "feature_plot_group_%s" % str(group)
+        self.readme_list.append("  %s\n" % plabel)
+        pdict=dict()
+        group_notelist=list()
+        if not(self.plot_filter_out is None):
             group_notelist.append("Data not displayed:")
             for pfstr in self.plot_filter_out:
                 group_notelist.append(pfstr.replace(";"," "))
-        if show_training:
-            label = self.data_labels[0] #training label
-            edict[label] = dict()
-            if use_filters:
-                if group is None:
-                    use_index = self.plot_filter_dict[label]['nogroup']
-                else:
-                    use_index = self.plot_filter_dict[label][group]
+        testsets = list(self.testing_dataset_dict.keys())
+        testsets.sort()
+        for testset in testsets:
+            ts_dict = self.testing_dataset_dict[testset]
+            ts_sf = ts_dict['SingleFit']
+            if ts_sf.plot_filter_out is None:
+                plotting_index = np.arange(0, len(ts_sf.testing_target_prediction))
             else:
-                use_index = np.arange(0, len(np.asarray(self.training_dataset[0].get_data(self.target_feature)).ravel()))
-            edict[label]['xdata'] = np.asarray(self.training_dataset[0].get_data(self.feature_plot_field)).ravel()[use_index]
-            edict[label]['xerrdata'] = None
-            edict[label]['ydata'] = np.asarray(self.training_dataset[0].get_data(self.target_feature)).ravel()[use_index]
-        for label in self.extrapolation_dict.keys():
-            s_analysis = self.extrapolation_dict[label].overall_analysis
-            if use_filters:
-                if group is None:
-                    use_index = self.plot_filter_dict[label]['nogroup']
-                else:
-                    use_index = self.plot_filter_dict[label][group]
+                plotting_index = ts_sf.plotting_index
+            if plotting_index is None:
+                print("NO PLOTTING INDEX: set %s, group %s" % (testset, group))
+            if group is None:
+                group_index = np.arange(0, len(ts_sf.testing_target_prediction))
             else:
-                use_index = np.arange(0, s_analysis.testing_input_data.shape[0])
-            if not(s_analysis.testing_target_data is None):
-                mlabel = "%s measured" % label
-                edict[mlabel] = dict()
-                edict[mlabel]['xdata'] = np.asarray(s_analysis.testing_dataset.get_data(self.feature_plot_field)).ravel()[use_index]
-                edict[mlabel]['xerrdata'] = None
-                edict[mlabel]['ydata'] = s_analysis.testing_target_data[use_index]
-            slabel = "%s predicted" % label
-            edict[slabel] = dict()
-            edict[slabel]['xdata'] = np.asarray(s_analysis.testing_dataset.get_data(self.feature_plot_field)).ravel()[use_index]
-            edict[slabel]['xerrdata'] = None
-            edict[slabel]['ydata'] = s_analysis.testing_target_prediction[use_index]
-        series_list = list(edict.keys())
+                if not group in ts_dict['group_indices'].keys():
+                    logging.info("No group %s for test set %s. Skipping." % (group, testset))
+                    continue
+                group_index = ts_dict['group_indices'][group]['test_index']
+            print("Group index, plotting index for group %s" % group)
+            print(group_index)
+            print(plotting_index)
+            display_index = np.intersect1d(group_index, plotting_index)
+            feature_data = ts_dict['feature_data']
+            measured = ts_sf.testing_target_data
+            predicted = ts_sf.testing_target_prediction
+            if measured is None:
+                pass
+            elif len(measured) == 0:
+                pass
+            else:
+                series_label = "%s, measured" % testset
+                pdict[series_label] = dict()
+                pdict[series_label]['xdata'] = feature_data
+                pdict[series_label]['xerrdata'] = None
+                pdict[series_label]['ydata'] = measured
+            if len(predicted) == 0:
+                pass
+            else:
+                series_label = "%s, predicted" % testset
+                pdict[series_label] = dict()
+                pdict[series_label]['xdata'] = feature_data
+                pdict[series_label]['xerrdata'] = None
+                pdict[series_label]['ydata'] = predicted
+        series_list = list(pdict.keys())
         if len(series_list) == 0:
             logging.info("No series for plot.")
             return
@@ -339,7 +240,7 @@ class PredictionVsFeature(SingleFitGrouped):
         for fidx in range(0, len(self.markers)):
             faces.append("None")
         addl_kwargs['faces'] = faces
-        plotdict.plot_group_splits_with_outliers(group_dict=edict,
+        plotdict.plot_group_splits_with_outliers(group_dict=pdict,
             outlying_groups = series_list,
             label=plabel,
             group_notelist = list(group_notelist),
