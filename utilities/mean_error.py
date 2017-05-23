9cc7461b0c560f02d56ac5e82a43c4256b773281
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-05-23 12:32:22 -0500

    mean_error unnecessary?; just use np.mean(pred-true)

--- utilities/mean_error.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def mean_error(prediction,actual):
-    pred_len = len(prediction)
-    errors = prediction - actual
-    sum_errors = np.sum(errors)
-    mean_error = sum_errors / pred_len
-    return mean_error
