bd741fae8e447bd8d6a55d61829b863b0dfb3c9f
rjacobs914 <rjacobs3@wisc.edu>
2017-05-31 11:58:54 -0500

    added remove constant features

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -3,23 +3,27 @@ __author__ = 'Ryan Jacobs'
 from sklearn.decomposition import PCA
 from DataOperations import DataframeUtilities
 from FeatureOperations import FeatureIO
-import numpy as np
-
+from sklearn.feature_selection import SelectKBest, f_classif, f_regression
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import VarianceThreshold
-from sklearn import svm
-from sklearn.feature_selection import SelectKBest, f_classif, f_regression
 
-class PrincipalComponentAnalysis(object):
-    """Class to conduct PCA for dimensional reduction of features. Mind that PCA produces linear combinations of features,
+
+class DimensionalReduction(object):
+    """Class to conduct PCA and constant feature removal for dimensional reduction of features. Mind that PCA produces linear combinations of features,
     and thus the resulting new features don't have physical meaning.
     """
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, x_features, y_feature):
         self.dataframe = dataframe
+        self.x_features = x_features
+        self.y_feature = y_feature
 
     @property
     def get_original_dataframe(self):
         return self.dataframe
+
+    def remove_constant_features(self):
+        dataframe = self.dataframe.loc[:, self.dataframe.var() != 0.0]
+        return dataframe
 
     def principal_component_analysis(self, x_features, y_feature):
         pca = PCA(n_components=len(x_features), svd_solver='auto')
@@ -31,26 +35,28 @@ class PrincipalComponentAnalysis(object):
 class ClassificationFeatureSelection(object):
     """Class to conduct feature selection routines to reduce the number of input features for a classification problem.
     """
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, x_features, y_feature):
         self.dataframe = dataframe
+        self.x_features = x_features
+        self.y_feature = y_feature
 
     @property
     def get_original_dataframe(self):
         return self.dataframe
 
-    def univariate_feature_selection(self, x_features, y_feature, features_to_keep):
+    def univariate_feature_selection(self, features_to_keep):
         selector = SelectKBest(score_func=f_classif, k=features_to_keep)
-        Xnew = selector.fit_transform(X=self.dataframe[x_features], y=self.dataframe[y_feature])
+        Xnew = selector.fit_transform(X=self.dataframe[self.x_features], y=self.dataframe[self.y_feature])
         feature_indices_selected = selector.get_support(indices=True)
         # Get the names of the features based on their indices, for features selected from feature selection
         feature_names_selected = []
-        for i in range(len(x_features)):
+        for i in range(len(self.x_features)):
             if i in feature_indices_selected:
-                feature_names_selected.append(x_features[i])
+                feature_names_selected.append(self.x_features[i])
         dataframe = DataframeUtilities()._array_to_dataframe(array=Xnew)
-        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=y_feature, remove_first_row=False)
+        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=self.y_feature, remove_first_row=False)
         # Add y_feature back into the dataframe
-        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[y_feature],data_to_add=self.dataframe[y_feature])
+        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[self.y_feature],data_to_add=self.dataframe[self.y_feature])
         return dataframe
 
     def recursive_feature_selection(self):
@@ -59,24 +65,26 @@ class ClassificationFeatureSelection(object):
 class RegressionFeatureSelection(object):
     """Class to conduct feature selection routines to reduce the number of input features for a regression problem.
     """
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, x_features, y_feature):
         self.dataframe = dataframe
+        self.x_features = x_features
+        self.y_feature = y_feature
 
     @property
     def get_original_dataframe(self):
         return self.dataframe
 
-    def univariate_feature_selection(self, x_features, y_feature, features_to_keep):
+    def univariate_feature_selection(self, features_to_keep):
         selector = SelectKBest(score_func=f_regression, k=features_to_keep)
-        Xnew = selector.fit_transform(X=self.dataframe[x_features], y=self.dataframe[y_feature])
+        Xnew = selector.fit_transform(X=self.dataframe[self.x_features], y=self.dataframe[self.y_feature])
         feature_indices_selected = selector.get_support(indices=True)
         # Get the names of the features based on their indices, for features selected from feature selection
         feature_names_selected = []
-        for i in range(len(x_features)):
+        for i in range(len(self.x_features)):
             if i in feature_indices_selected:
-                feature_names_selected.append(x_features[i])
+                feature_names_selected.append(self.x_features[i])
         dataframe = DataframeUtilities()._array_to_dataframe(array=Xnew)
-        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=y_feature, remove_first_row=False)
+        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=self.y_feature, remove_first_row=False)
         # Add y_feature back into the dataframe
-        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[y_feature],data_to_add=self.dataframe[y_feature])
+        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[self.y_feature],data_to_add=self.dataframe[self.y_feature])
         return dataframe
