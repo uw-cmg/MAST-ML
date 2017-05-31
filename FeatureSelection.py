ae8103cf3810ff9b9400ab54faa2794861d5a9af
rjacobs914 <rjacobs3@wisc.edu>
2017-05-31 11:39:19 -0500

    regression univariate feature selection works

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -3,8 +3,12 @@ __author__ = 'Ryan Jacobs'
 from sklearn.decomposition import PCA
 from DataOperations import DataframeUtilities
 from FeatureOperations import FeatureIO
+import numpy as np
+
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import VarianceThreshold
+from sklearn import svm
+from sklearn.feature_selection import SelectKBest, f_classif, f_regression
 
 class PrincipalComponentAnalysis(object):
     """Class to conduct PCA for dimensional reduction of features. Mind that PCA produces linear combinations of features,
@@ -34,6 +38,19 @@ class ClassificationFeatureSelection(object):
     def get_original_dataframe(self):
         return self.dataframe
 
+    def univariate_feature_selection(self, x_features, y_feature):
+        selector = SelectKBest(score_func=f_classif, k=10)
+        print(self.dataframe[x_features].shape)
+        Xnew = selector.fit_transform(X=self.dataframe[x_features], y=self.dataframe[y_feature])
+        print(Xnew.shape)
+        df_Xnew = DataframeUtilities()._array_to_dataframe(array=Xnew)
+        print(df_Xnew)
+        Xtrans = selector.inverse_transform(X=Xnew)
+        print(Xtrans.shape)
+        df_Xtrans = DataframeUtilities()._array_to_dataframe(array=Xtrans)
+        print(df_Xtrans)
+        return
+
     def recursive_feature_selection(self):
         pass
 
@@ -46,3 +63,18 @@ class RegressionFeatureSelection(object):
     @property
     def get_original_dataframe(self):
         return self.dataframe
+
+    def univariate_feature_selection(self, x_features, y_feature, features_to_keep):
+        selector = SelectKBest(score_func=f_regression, k=features_to_keep)
+        Xnew = selector.fit_transform(X=self.dataframe[x_features], y=self.dataframe[y_feature])
+        feature_indices_selected = selector.get_support(indices=True)
+        # Get the names of the features based on their indices, for features selected from feature selection
+        feature_names_selected = []
+        for i in range(len(x_features)):
+            if i in feature_indices_selected:
+                feature_names_selected.append(x_features[i])
+        dataframe = DataframeUtilities()._array_to_dataframe(array=Xnew)
+        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=y_feature, remove_first_row=False)
+        # Add y_feature back into the dataframe
+        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[y_feature],data_to_add=self.dataframe[y_feature])
+        return dataframe
