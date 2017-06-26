7b813d80d82121a6747e17a611174360ca1a6676
rjacobs914 <rjacobs3@wisc.edu>
2017-06-26 16:03:05 -0500

    added forward selection. Rolled back other types until have time to add CV

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -14,6 +14,8 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import ExtraTreesRegressor
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+import pandas as pd
 
 class DimensionalReduction(object):
     """Class to conduct PCA and constant feature removal for dimensional reduction of features. Mind that PCA produces linear combinations of features,
@@ -56,36 +58,51 @@ class FeatureSelection(object):
     def get_original_dataframe(self):
         return self.dataframe
 
+    def forward_selection(self, number_features_to_keep, save_to_csv=True):
+        sfs = SFS(KernelRidge(), k_features=number_features_to_keep, forward=True, floating=False, verbose=0, scoring='r2', cv=ShuffleSplit(n_splits=5, test_size=0.2))
+        sfs = sfs.fit(X=np.array(self.dataframe[self.x_features]), y=np.array(self.dataframe[self.y_feature]))
+        Xnew = sfs.fit_transform(X=np.array(self.dataframe[self.x_features]), y=np.array(self.dataframe[self.y_feature]))
+        feature_indices_selected = sfs.k_feature_idx_
+        dataframe = DataframeUtilities()._array_to_dataframe(array=Xnew)
+        feature_names_selected = MiscFeatureSelectionOperations().get_forward_selection_feature_names(feature_indices_selected=feature_indices_selected, x_features=self.x_features)
+        dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=self.y_feature, remove_first_row=False)
+        # Add y_feature back into the dataframe
+        dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[self.y_feature],data_to_add=self.dataframe[self.y_feature])
+        dataframe = dataframe.dropna()
+        # Get forward selection data
+        fs_dataframe = pd.DataFrame.from_dict(sfs.get_metric_dict()).T
+        logging.info(("Summary of forward selection:"))
+        logging.info(fs_dataframe)
+        if save_to_csv == bool(True):
+            dataframe.to_csv('input_with_forward_selection.csv', index=False)
+            fs_dataframe.to_csv('foward_selection_data.csv', index=False)
+        return dataframe
+
+    """
     def univariate_feature_selection(self, number_features_to_keep, save_to_csv=True):
         if self.selection_type == 'Regression' or self.selection_type == 'regression':
+            Fscores, pvalues = f_regression(X=self.dataframe[self.x_features], y=self.dataframe[self.y_feature])
+            print('Fscores:')
+            print(Fscores)
             selector = SelectKBest(score_func=f_regression, k=number_features_to_keep)
         if self.selection_type == 'Classification' or self.selection_type == 'classification':
             selector = SelectKBest(score_func=f_classif, k=number_features_to_keep)
+
         Xnew = selector.fit_transform(X=self.dataframe[self.x_features], y=self.dataframe[self.y_feature])
+
+        #print('feature selection scores:')
+        #print(selector.scores_)
 
         feature_names_selected = MiscFeatureSelectionOperations().get_selector_feature_names(selector=selector, x_features=self.x_features)
         dataframe = DataframeUtilities()._array_to_dataframe(array=Xnew)
-
-        """
-        # Do 20 LeaveOut 20% CV runs
-        cv = ShuffleSplit(n_splits=20, test_size=0.2)
-        #estimator = KernelRidge(alpha=0.003, coef0=1, degree=3, gamma=3.47, kernel='rbf')
-        #estimator = KernelRidge(kernel='rbf')
-        estimator = ExtraTreesRegressor(criterion='mse', max_depth=17, min_samples_split=2, min_samples_leaf=1, n_estimators=135)
-        cv_scores = cross_val_score(estimator=estimator, X=Xnew, y=self.dataframe[self.y_feature], cv=cv, scoring='r2')
-        cv_scores_mean = np.mean(cv_scores)
-        cv_scores_stdev = np.std(cv_scores)
-        print('CV scores:', cv_scores)
-        print('CV mean:', cv_scores_mean)
-        print('CV stdev:', cv_scores_stdev)
-        #self.plot_learning_curve(estimator=estimator, title='Univariate feature selection learning curve', X=Xnew, y=self.dataframe[self.y_feature], cv=cv, n_jobs=1)
-        """
 
         dataframe = DataframeUtilities()._assign_columns_as_features(dataframe=dataframe, x_features=feature_names_selected, y_feature=self.y_feature, remove_first_row=False)
         # Add y_feature back into the dataframe
         dataframe = FeatureIO(dataframe=dataframe).add_custom_features(features_to_add=[self.y_feature],data_to_add=self.dataframe[self.y_feature])
         if save_to_csv == bool(True):
             dataframe.to_csv('input_with_univariate_feature_selection.csv', index=False)
+
+        dataframe = dataframe.dropna()
         return dataframe
 
     def recursive_feature_elimination(self, number_features_to_keep, save_to_csv=True):
@@ -94,6 +111,7 @@ class FeatureSelection(object):
         if self.selection_type == 'Classification' or self.selection_type == 'classification':
             estimator = SVC(kernel='linear')
         selector = RFE(estimator=estimator, n_features_to_select=number_features_to_keep)
+
         Xnew = selector.fit_transform(X=self.dataframe[self.x_features], y=self.dataframe[self.y_feature])
         feature_names_selected = MiscFeatureSelectionOperations().get_selector_feature_names(selector=selector, x_features=self.x_features)
         #feature_names_selected = MiscOperations().get_ranked_feature_names(selector=selector, x_features=self.x_features, number_features_to_keep=number_features_to_keep)
@@ -119,38 +137,8 @@ class FeatureSelection(object):
         if save_to_csv == bool(True):
             dataframe.to_csv('input_with_stability_feature_selection.csv', index=False)
         return dataframe
-
     """
-    def plot_learning_curve(self, estimator, title, X, y, ylim=None, cv=None,
-                            n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
-        plt.figure()
-        plt.title(title)
-        if ylim is not None:
-            plt.ylim(*ylim)
-        plt.xlabel("Training examples")
-        plt.ylabel("Score")
-        train_sizes, train_scores, test_scores = learning_curve(
-            estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
-        train_scores_mean = np.mean(train_scores, axis=1)
-        train_scores_std = np.std(train_scores, axis=1)
-        test_scores_mean = np.mean(test_scores, axis=1)
-        test_scores_std = np.std(test_scores, axis=1)
-        plt.grid()
 
-        plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-        plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1, color="g")
-        plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-        plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Cross-validation score")
-
-        plt.legend(loc="best")
-        plt.show()
-        return plt
-    """
 
 class MiscFeatureSelectionOperations():
 
@@ -163,6 +151,16 @@ class MiscFeatureSelectionOperations():
             if i in feature_indices_selected:
                 feature_names_selected.append(x_features[i])
         return feature_names_selected
+
+    @classmethod
+    def get_forward_selection_feature_names(cls, feature_indices_selected, x_features):
+        # Get the names of the features based on their indices, for features selected from feature selection
+        feature_names_selected = []
+        for i in range(len(x_features)):
+            if i in feature_indices_selected:
+                feature_names_selected.append(x_features[i])
+        return feature_names_selected
+
 
     @classmethod
     def get_ranked_feature_names(cls, selector, x_features, number_features_to_keep):
