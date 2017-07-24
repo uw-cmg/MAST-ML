45f143f9a4a4a38c39c0409ea96a65c644ccaa5c
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-07-24 10:15:15 -0500

    Clean up format and add eqn columns

--- custom_models/DBTT_E900.py
+++ custom_models/DBTT_E900.py
@@ -9,83 +9,99 @@ import os
 class E900model(BaseCustomModel):
     def __init__(self, **kwargs):
         """
-            self.wtP: feature name for weight percentage of P
-            self.wtNi
-            self.wtMn
-            self.wtCu
-            self.fluencestr: feature name for fluence in n/cm2
-            self.tempC: feature name for temperature in C
-            self.prod_ID: feature name for product ID (plate, weld,
+            self.wtP_feature: feature name for weight percentage of P
+            self.wtNi_feature
+            self.wtMn_feature
+            self.wtCu_feature
+            self.fluence_n_cm2_feature: feature name for fluence in n/cm2
+            self.temp_C_feature: feature name for temperature in C
+            self.product_id_feature: feature name for product ID (plate, weld,
                             forging, etc.)
         """
-        self.wtP="P"
-        self.wtNi="Ni"
-        self.wtMn="Mn"
-        self.wtCu="Cu"
-        self.fluencestr="fluence_n_cm2"
-        self.tempC="temperature_C"
-        self.prod_ID="Product ID"
+        self.wtP_feature="P"
+        self.wtNi_feature="Ni"
+        self.wtMn_feature="Mn"
+        self.wtCu_feature="Cu"
+        self.fluence_n_cm2_feature="fluence_n_cm2"
+        self.temp_C_feature="temperature_C"
+        self.product_id_feature="Product ID"
         BaseCustomModel.__init__(self, **kwargs)
         return
 
     def predict(self, input_data):
         """E900 model, ASTM E900-15
-            CC conversion of TTS to delta sigma Y is from ORNL/TM-2006/530
+            Cc conversion of TTS to delta sigma Y is from ORNL/TM-2006/530
             equation 6-3 and 6-4.
         """
-        p = input_data[self.wtP]
-        ni = input_data[self.wtNi]
-        mn = input_data[self.wtMn]
-        cu = input_data[self.wtCu]
-        tempc = input_data[self.tempC]
-
-        pdf = pd.DataFrame(index=input_data.index, columns=['Acol','Bcol','cc'])
-        pdf['fluence_n_m2'] = input_data[self.fluencestr] * 100.0 * 100.0
-
-        plates_idx = input_data[input_data[self.prod_ID] == "P"].index
-        forgings_idx = input_data[input_data[self.prod_ID] == "F"].index
-        srm_plates_idx = input_data[input_data[self.prod_ID] == "SRM"].index
-        weldings_idx = input_data[input_data[self.prod_ID] == "W"].index
-
-        for idx in plates_idx:
-            pdf.set_value(idx, 'Acol', 1.080)
-            pdf.set_value(idx, 'Bcol', 0.819)
-        for idx in forgings_idx:
-            pdf.set_value(idx, 'Acol', 1.011)
-            pdf.set_value(idx, 'Bcol', 0.738)
-        for idx in srm_plates_idx:
-            pdf.set_value(idx, 'Acol', 1.080) #standard reference material plate
-            pdf.set_value(idx, 'Bcol', 0.819)
-        for idx in weldings_idx:
-            pdf.set_value(idx, 'Acol', 0.919)
-            pdf.set_value(idx, 'Bcol', 0.968)
-
-        pdf['tts1'] = pdf.Acol * (5./9.) * 1.8943 * np.power(10.,-12.) * np.power(pdf['fluence_n_m2'],0.5695) * np.power(((1.8 * input_data[self.tempC] + 32.0)/550.0),-5.47) * np.power((0.09 + input_data[self.wtP]/0.012),0.216) * np.power((1.66 + (np.power(input_data[self.wtNi],8.54))/0.63),0.39) * np.power((input_data[self.wtMn]/1.36),0.3)
-        
-        pdf['Mcol'] = pdf.Bcol * np.maximum(np.minimum(113.87 * (np.log(pdf['fluence_n_m2']) - np.log(4.5 * np.power(10.0,20.0))), 612.6), 0.) * np.power(((1.8 * input_data[self.tempC] + 32.0)/550.0), -5.45) * np.power((0.1 + input_data[self.wtP]/0.012),-0.098) * np.power((0.168 + np.power(input_data[self.wtNi],0.58)/0.63),0.73)
-
-        pdf['tts2'] = (5./9.) * np.maximum(np.minimum(cu, 0.28) - 0.053,0) * pdf.Mcol
-
-        pdf['tts'] = pdf.tts1 + pdf.tts2
-
-        pdf.cc[weldings_idx] = 0.55 + (1.2e-3) * pdf.tts[weldings_idx] - 1.33e-6 * np.power(pdf.tts[weldings_idx],2.0)
-        pdf.cc[plates_idx] = 0.45 + (1.945e-3) * pdf.tts[plates_idx] - 5.496e-6 * np.power(pdf.tts[plates_idx],2.0) + 8.473e-9 * np.power(pdf.tts[plates_idx],3.0)
-        pdf.cc[srm_plates_idx] = 0.45 + (1.945e-3) * pdf.tts[srm_plates_idx] - 5.496e-6 * np.power(pdf.tts[srm_plates_idx],2.0) + 8.473e-9 * np.power(pdf.tts[srm_plates_idx],3.0)
-        
-        pdf.cc[forgings_idx] = 0.45 + (1.945e-3) * pdf.tts[forgings_idx] - 5.496e-6 * np.power(pdf.tts[forgings_idx],2.0) + 8.473e-9 * np.power(pdf.tts[forgings_idx],3.0)
-        
-        pdf['ds'] = pdf['tts'] / pdf['cc']
+        # Set up working dataframe
+        empty_cols=list()
+        empty_cols.append("Acol")
+        empty_cols.append("Bcol")
+        empty_cols.append("Cc")
+        pdf = pd.DataFrame(index=input_data.index, columns=empty_cols)
         pdf = pdf.merge(input_data, left_index=True, right_index=True)
+        # Set up shortcut variables 
+        wtP = pdf[self.wtP_feature]
+        wtNi = pdf[self.wtNi_feature]
+        wtMn = pdf[self.wtMn_feature]
+        wtCu = pdf[self.wtCu_feature]
+        tempC = pdf[self.temp_C_feature]
+        product = pdf[self.product_id_feature]
+        fluence_n_cm2 = pdf[self.fluence_n_cm2_feature]
+        # Convert fluence
+        pdf["fluence_n_m2"] = fluence_n_cm2 * 100.0 * 100.0
+        # Get product indices
+        plates_idx = pdf[product == "P"].index
+        forgings_idx = pdf[product == "F"].index
+        srm_plates_idx = pdf[product == "SRM"].index #standard ref. matl plates
+        welds_idx = pdf[product == "W"].index
+        # Set Acol according to product type
+        pdf.loc[forgings_idx, 'Acol'] = 1.011
+        pdf.loc[plates_idx, 'Acol'] = 1.080
+        pdf.loc[srm_plates_idx, 'Acol'] = 1.080
+        pdf.loc[welds_idx, 'Acol'] = 0.919
+        # Set Bcol according to product type
+        pdf.loc[forgings_idx, 'Bcol'] = 0.738
+        pdf.loc[plates_idx, 'Bcol'] = 0.819
+        pdf.loc[srm_plates_idx, 'Bcol'] = 0.819
+        pdf.loc[welds_idx, 'Bcol'] = 0.968
+        # Get TTS1
+        pdf['TTS1_front'] = pdf.Acol * (5./9.) * 1.8943e-12 * np.power(pdf.fluence_n_m2, 0.5695)
+        pdf['TTS1_temp'] = np.power( ((1.8 * tempC + 32.)/550.) , -5.47)
+        pdf['TTS1_P'] = np.power( (0.09 + (wtP/0.012)) , 0.216)
+        pdf['TTS1_Ni'] = np.power( (1.66 + (np.power(wtNi, 8.54)/0.63)) , 0.39)
+        pdf['TTS1_Mn'] = np.power( (wtMn/1.36), 0.3)
+        pdf['TTS1'] = pdf.TTS1_front * pdf.TTS1_temp * pdf.TTS1_P * pdf.TTS1_Ni * pdf.TTS1_Mn
+        # Get TTS2
+        pdf['TTS2_front'] = (5./9.)
+        pdf['TTS2_Cu'] = np.maximum(np.minimum(wtCu, 0.28) - 0.053, 0)
+        pdf['M_flux'] = np.maximum(np.minimum(113.87 * (np.log(pdf.fluence_n_m2) - np.log(4.5e20)), 612.6), 0)
+        pdf['M_temp'] = np.power( ((1.8*tempC + 32.0)/550.) , -5.45)
+        pdf['M_P'] = np.power( (0.1 + (wtP/0.012)), -0.098)
+        pdf['M_Ni'] = np.power( (0.168 + (np.power(wtNi, 0.58)/0.63)), 0.73)
+        pdf['Mcol'] = pdf.Bcol * pdf.M_flux * pdf.M_temp * pdf.M_P * pdf.M_Ni
+        pdf['TTS2'] = pdf.TTS2_front * pdf.TTS2_Cu * pdf.Mcol
+        # Get TTS
+        pdf['TTS'] = pdf.TTS1 + pdf.TTS2
+        # Get Cc according to product type
+        c_welds = [0.55, 1.2e-3, -1.33e-6, 0.0]
+        c_plates = [0.45, 1.945e-3, -5.496e-6, 8.473e-9]
+        # Cc default to plates
+        pdf.Cc = c_plates[0] + c_plates[1]*pdf.TTS + c_plates[2]*np.power(pdf.TTS, 2.0) + c_plates[3]*np.power(pdf.TTS, 3.0)
+        # Cc for welds
+        pdf.loc[welds_idx, 'Cc'] = c_welds[0] + c_welds[1]*pdf.TTS[welds_idx] + c_welds[2]*np.power(pdf.TTS[welds_idx], 2.0) + c_welds[3]*np.power(pdf.TTS[welds_idx], 3.0)
+        # Get hardening (delta_sigma_y_MPa)
+        pdf['delta_sigma_y_MPa'] = pdf.TTS / pdf.Cc
         pdf.to_csv(os.path.join(os.getcwd(),"E900.csv"))
-        return pdf['ds']
+        return pdf['delta_sigma_y_MPa']
         
     def get_params(self):
         param_dict=dict()
-        param_dict['wtP'] = self.wtP
-        param_dict['wtNi'] = self.wtNi
-        param_dict['wtMn'] = self.wtMn
-        param_dict['wtCu'] = self.wtCu
-        param_dict['fluencestr'] = self.fluencestr
-        param_dict['tempC'] = self.tempC
-        param_dict['prod_ID'] = self.prod_ID
+        param_dict['wtP_feature'] = self.wtP_feature
+        param_dict['wtNi_feature'] = self.wtNi_feature
+        param_dict['wtMn_feature'] = self.wtMn_feature
+        param_dict['wtCu_feature'] = self.wtCu_feature
+        param_dict['fluence_n_cm2_feature'] = self.fluence_n_cm2_feature
+        param_dict['temp_C_feature'] = self.temp_C_feature
+        param_dict['product_id_feature'] = self.product_id_feature
         return param_dict
