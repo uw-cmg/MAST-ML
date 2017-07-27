fadb1264b990a5e11f3f5e38f2aab2805331c54f
Tam Mayeshiba <mayeshiba@wisc.edu>
2017-07-27 12:01:07 -0500

    Remove unused code

--- ParamGridSearch.py
+++ ParamGridSearch.py
@@ -450,19 +450,6 @@ class ParamGridSearch(SingleFit):
         self.pop_size = pop_size
         return
 
-    @timeit
-    def fit(self):
-        self.cv_fit_and_predict()
-        return
-
-    @timeit
-    def predict(self):
-        #Predictions themselves are covered in self.fit()
-        self.get_statistics()
-        self.print_statistics()
-        self.readme_list.append("----- Output data -----\n")
-        self.print_best_worst_output_csv("best_and_worst")
-        return
 
     @timeit
     def plot(self):
@@ -591,8 +578,6 @@ class ParamGridSearch(SingleFit):
         self.readme_list.append("Plot %s.png created\n" % plotlabel)
         return
 
-    def plot_pair(self):
-        return
 
     def flatten_results(self):
         """Flatten results into a csv
@@ -619,104 +604,4 @@ class ParamGridSearch(SingleFit):
         self.flat_results = flat_results
         return
 
-    def set_up_cv(self):
-        if self.testing_dataset.target_data is None:
-            raise ValueError("Testing target data cannot be none for cross validation.")
-        indices = np.arange(0, len(self.testing_dataset.target_data))
-        self.readme_list.append("----- CV setup -----\n")
-        self.readme_list.append("%i CV tests,\n" % self.num_cvtests)
-        self.readme_list.append("leaving out %i percent\n" % self.percent_leave_out)
-        test_fraction = self.percent_leave_out / 100.0
-        self.cvmodel = ShuffleSplit(n_splits = 1, 
-                            test_size = test_fraction, 
-                            random_state = None)
-        for cvtest in range(0, self.num_cvtests):
-            self.cvtest_dict[cvtest] = dict()
-            for train, test in self.cvmodel.split(indices):
-                fdict=dict()
-                fdict['train_index'] = train
-                fdict['test_index'] = test
-                self.cvtest_dict[cvtest]= dict(fdict)
-        return
-    
-    def cv_fit_and_predict(self):
-        for cvtest in self.cvtest_dict.keys():
-            prediction_array = np.zeros(len(self.testing_dataset.target_data))
-            prediction_array[:] = np.nan
-            fdict = self.cvtest_dict[cvtest]
-            input_train=self.testing_dataset.input_data.iloc[fdict['train_index']]
-            target_train = self.testing_dataset.target_data[fdict['train_index']]
-            input_test=self.testing_dataset.input_data.iloc[fdict['test_index']]
-            target_test = self.testing_dataset.target_data[fdict['test_index']]
-            fit = self.model.fit(input_train, target_train)
-            predict_test = self.model.predict(input_test)
-            rmse = np.sqrt(mean_squared_error(predict_test, target_test))
-            merr = np.mean(predict_test - target_test)
-            prediction_array[fdict['test_index']] = predict_test
-            self.cvtest_dict[cvtest]["rmse"] = rmse
-            self.cvtest_dict[cvtest]["mean_error"] = merr
-            self.cvtest_dict[cvtest]["prediction_array"] = prediction_array
-        return
-
-    def get_statistics(self):
-        cvtest_rmses = list()
-        cvtest_mean_errors = list()
-        for cvtest in range(0, self.num_cvtests):
-            cvtest_rmses.append(self.cvtest_dict[cvtest]["rmse"])
-            cvtest_mean_errors.append(self.cvtest_dict[cvtest]["mean_error"])
-        highest_rmse = max(cvtest_rmses)
-        self.worst_test_index = cvtest_rmses.index(highest_rmse)
-        lowest_rmse = min(cvtest_rmses)
-        self.best_test_index = cvtest_rmses.index(lowest_rmse)
-        self.statistics['avg_rmse'] = np.mean(cvtest_rmses)
-        self.statistics['std_rmse'] = np.std(cvtest_rmses)
-        self.statistics['avg_mean_error'] = np.mean(cvtest_mean_errors)
-        self.statistics['std_mean_error'] = np.std(cvtest_mean_errors)
-        self.statistics['rmse_best'] = lowest_rmse
-        self.statistics['rmse_worst'] = highest_rmse
-        return
-
-    def print_best_worst_output_csv(self, label=""):
-        """
-        """
-        olabel = "%s_test_data.csv" % label
-        ocsvname = os.path.join(self.save_path, olabel)
-        self.testing_dataset.add_feature("Best Prediction", 
-                    self.cvtest_dict[self.best_test_index]['prediction_array'])
-        self.testing_dataset.add_feature("Worst Prediction", 
-                    self.cvtest_dict[self.worst_test_index]['prediction_array'])
-        cols = self.testing_dataset.print_data(ocsvname, ["Best Prediction", "Worst Prediction"])
-        self.readme_list.append("%s file created with columns:\n" % olabel)
-        for col in cols:
-            self.readme_list.append("    %s\n" % col)
-        return
-
-
-    def plot_best_worst_overlay(self, notelist=list()):
-        kwargs2 = dict()
-        kwargs2['xlabel'] = self.xlabel
-        kwargs2['ylabel'] = self.ylabel
-        kwargs2['labellist'] = ["Best test","Worst test"]
-        kwargs2['xdatalist'] = list([self.testing_dataset.target_data, 
-                            self.testing_dataset.target_data])
-        kwargs2['ydatalist'] = list(
-                [self.cvtest_dict[self.best_test_index]['prediction_array'],
-                self.cvtest_dict[self.worst_test_index]['prediction_array']])
-        kwargs2['xerrlist'] = list([None,None])
-        kwargs2['yerrlist'] = list([None,None])
-        kwargs2['notelist'] = list(notelist)
-        kwargs2['guideline'] = 1
-        kwargs2['plotlabel'] = "best_worst_overlay"
-        kwargs2['save_path'] = self.save_path
-        if not (self.mark_outlying_points is None):
-            kwargs2['marklargest'] = self.mark_outlying_points
-            if self.testing_dataset.labeling_features is None:
-                raise ValueError("Must specify some labeling features if you want to mark the largest outlying points")
-            labels = self.testing_dataset.data[self.testing_dataset.labeling_features[0]]
-            kwargs2['mlabellist'] = list([labels,labels])
-        myph = PlotHelper(**kwargs2)
-        myph.multiple_overlay()
-        self.readme_list.append("Plot best_worst_overlay.png created,\n")
-        self.readme_list.append("    showing the best and worst of %i tests.\n" % self.num_cvtests)
-        return
 
