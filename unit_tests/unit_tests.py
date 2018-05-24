d35e687ca8270835fae1867580ffe9e5cafa850c
Robert Max Williams <robertmaxwilliams@gmail.com>
2018-05-24 10:39:16 -0400

    started MASTMLInitializer unit tests

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -8,9 +8,10 @@ import pdb
 # so we can find MASTML package
 sys.path.append('../')
 
-from MASTML import MASTMLDriver
+import MASTML
+import MASTMLInitializer
 
-class TestMASTML(unittest.TestCase):
+class SmokeTest(unittest.TestCase):
 
     def setUp(self):
         self.folders = list()
@@ -18,7 +19,7 @@ class TestMASTML(unittest.TestCase):
     def test_full_run(self):
         configfile = 'full_run.conf'
         warnings.simplefilter('ignore')
-        MASTMLDriver(configfile=configfile).run_MASTML()
+        MASTML.MASTMLDriver(configfile=configfile).run_MASTML()
         self.folders.append('results/full_run')
 
     def test_basic_example(self):
@@ -38,6 +39,49 @@ class TestMASTML(unittest.TestCase):
             pass
             # uncomment for no leftover folders:
             #shutil.rmtree(f)
+def leaves_of_dict(dictt):
+    """ 
+    Returns non-dict elements of dict recursively, as a generator
+    """
+    for key, val in dictt.items():
+        if isinstance(val, dict):
+            yield from leaves_of_dict(val)
+        else:
+            yield val
+
+class ParseTemplate(unittest.TestCase):
+
+    def setUp(self):
+        template_folder  = '../MASTML_input_file_template/'
+        template_file = 'MASTML_input_file_template.conf'
+        # note: template is the name of the file, don't confuse with template
+        # as in typing template in MASTMLInitializer.
+        self.parser = MASTMLInitializer.ConfigFileParser(template_file)
+        self.config_dict = self.parser.get_config_dict(template_folder)
+
+    def test_dict_is_not_empty(self):
+        self.assertTrue(0 < len(self.config_dict.keys()))
+
+    def test_dict_values_are_lists_or_strings(self):
+        for leaf in leaves_of_dict(self.config_dict):
+             # leaf is leaf in the dict way, not in the list way.
+             # check if leaf is string or every member of leaf is string
+             self.assertTrue(isinstance(leaf, str) or
+                             all(isinstance(x, str) for x in leaf))
+
+ 
+    def test_type_checking_template
+
+    def tearDown(self):
+        pass
+
+class TestConfigKeys(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
 
 if __name__=='__main__':
     unittest.main()
