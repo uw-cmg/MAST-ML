214d4b01d57b641eb58f92fa7b378a4a83c0776b
Luke Miles and Robert Max Williams <robertmaxwilliams@gmail.com>
2018-05-31 11:43:19 -0400

    improved confusion_matrix plots

--- MultiClass.py
+++ MultiClass.py
@@ -35,17 +35,8 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     """
 
     plt.figure(figsize=(8,6))
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
-    plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
