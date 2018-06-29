57cedfb3833846d03ce050c6f964e784215b4cb0
Luke Harold Miles <luke.lambda@uky.edu>
2018-06-29 01:18:00 -0400

    more code for search.py...

--- mastml/search/search.py
+++ mastml/search/search.py
@@ -8,11 +8,29 @@ from .data_handler import DataHandler
 from ..legos import model_finder
 from .. import mastml, utils
 
+genetic_search_user_params = [ # parameters to GridSearch initializer which user has permission to set
+    'model', 'xlabel', 'ylabel', 'fix_random_for_testing', 'num_cvtests', 'mark_outlying_points',
+    'num_folds', 'percent_leave_out', 'processors', 'pop_upper_limit', 'num_bests',
+]
+
+genetic_search_user_params = [ # parameters to GeneticSearch initializer which user has permission to set
+    'model', 'num_folds', 'percent_leave_out', 'num_cvtests', 'mark_outlying_points', 'num_bests',
+    'fix_random_for_testing', 'processors', 'pop_upper_limit', 'num_gas', 'ga_pop_size',
+    'convergence_generations', 'max_generations', 'crossover_prob', 'mutation_prob', 'shift_prob', 'gen_tol',
+]
+
 def parse_conf_file(filepath):
     "Accepts the filepath of a conf file and returns its parsed dictionary"
     conf = ConfigObj(filepath)
     # TODO
 
+def do_run(conf_path, data_path, outdir):
+    conf = parse_conf_file(conf_path)
+    df = pd.read_csv(data_path)
+    # something like this:
+    #dh = DataHandler(df, input_features=['a','b'], target_feature='y')
+    #gs = GeneticSearch(params, dh, dh, model, './results/', num_folds=5, max_generations=1, ga_pop_size=3, processors=1)
+    #gs.run()
 
 
 
