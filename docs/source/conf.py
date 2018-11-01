33515282229028bb1fedcdac92e5dff70c6b4047
rjacobs914 <rjacobs3@wisc.edu>
2018-11-01 15:48:19 -0500

    change to classic theme as github couldn't use sphinx rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -108,11 +109,11 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
+        #'about.html',
+        #'navigation.html',
+        #'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
-        'donate.html',
+        #'donate.html',
     ]
 }
 
