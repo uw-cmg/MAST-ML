37f7ccfd11e55120e71aa2764b4169b66e73d415
Alex Do <alex.do21@yahoo.com>
2019-05-02 11:13:58 -0500

    fixed GlobalSymmetryFeatures is_centrosymmetric T/F 1/0

--- mastml/legos/feature_generators.py
+++ mastml/legos/feature_generators.py
@@ -91,13 +91,11 @@ class Matminer(BaseEstimator, TransformerMixin):
                     sf = getattr(struc, self.structural_features[struc_feat])()  # instantiates the structure featurizer
                     df = sf.fit_featurize_dataframe(df, self.structure_col)  # fit_featurize_dataframe() works for all
                     # updates dataframe if the structural feature happens to be the GlobalSymmetryFeatures
-                    if self.structural_features[struc_feat] == "GlobalSymmetryFeatures":
+                    if self.structural_features[struc_feat] == 'GlobalSymmetryFeatures':
                         df = df.drop('crystal_system', axis=1)
-                        for i, rows in df.iterrows():
-                            if df.at[i, "is_centrosymmetric"] == True:
-                                df.at[i, "is_centrosymmetric"] = 1
-                            elif df.at[i, "is_centrosymmetric"] == False:
-                                df.at[i, "is_centrosymmetric"] = 0
+                        df['is_centrosymmetric'].replace(True, 1, inplace=True)
+                        df['is_centrosymmetric'].replace(False, 0, inplace=True)
+                        
                     break # structure feature was found for this iteration, repeat
 
         # drop unused dataframe columns for rest of application
