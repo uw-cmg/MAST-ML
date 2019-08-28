e872279e63be510c2e53a5b85d3c32945c018d96
rjacobs914 <rjacobs3@wisc.edu>
2019-08-28 14:14:28 -0500

    Added bootstrapping method for data resampling. This is new data_splitter test!

--- mastml/legos/data_splitters.py
+++ mastml/legos/data_splitters.py
@@ -13,6 +13,10 @@ import sklearn.model_selection as ms
 from matminer.featurizers.composition import ElementFraction
 from pymatgen import Composition
 
+from math import ceil
+import warnings
+from sklearn.utils import check_random_state
+
 class SplittersUnion(BaseEstimator, TransformerMixin):
     """
     Class to take the union of two separate splitting routines, so that many splitting routines can be performed at once
@@ -228,8 +232,150 @@ class LeaveOutPercent(BaseEstimator, TransformerMixin):
             split.append((trains, tests))
         return split
 
+# Note: Bootstrap taken directly from sklearn Github (https://github.com/scikit-learn/scikit-learn/blob/0.11.X/sklearn/cross_validation.py)
+# which was necessary as it was later removed from more recent sklearn releases
+class Bootstrap(object):
+    """Random sampling with replacement cross-validation iterator
+    Provides train/test indices to split data in train test sets
+    while resampling the input n_bootstraps times: each time a new
+    random split of the data is performed and then samples are drawn
+    (with replacement) on each side of the split to build the training
+    and test sets.
+    Note: contrary to other cross-validation strategies, bootstrapping
+    will allow some samples to occur several times in each splits. However
+    a sample that occurs in the train split will never occur in the test
+    split and vice-versa.
+    If you want each sample to occur at most once you should probably
+    use ShuffleSplit cross validation instead.
+    Parameters
+    ----------
+    n : int
+        Total number of elements in the dataset.
+    n_bootstraps : int (default is 3)
+        Number of bootstrapping iterations
+    train_size : int or float (default is 0.5)
+        If int, number of samples to include in the training split
+        (should be smaller than the total number of samples passed
+        in the dataset).
+        If float, should be between 0.0 and 1.0 and represent the
+        proportion of the dataset to include in the train split.
+    test_size : int or float or None (default is None)
+        If int, number of samples to include in the training set
+        (should be smaller than the total number of samples passed
+        in the dataset).
+        If float, should be between 0.0 and 1.0 and represent the
+        proportion of the dataset to include in the test split.
+        If None, n_test is set as the complement of n_train.
+    random_state : int or RandomState
+        Pseudo number generator state used for random sampling.
+    Examples
+    --------
+    #>>> from sklearn import cross_validation
+    #>>> bs = cross_validation.Bootstrap(9, random_state=0)
+    #>>> len(bs)
+    3
+    #>>> print bs
+    Bootstrap(9, n_bootstraps=3, train_size=5, test_size=4, random_state=0)
+    #>>> for train_index, test_index in bs:
+    ...    print "TRAIN:", train_index, "TEST:", test_index
+    ...
+    TRAIN: [1 8 7 7 8] TEST: [0 3 0 5]
+    TRAIN: [5 4 2 4 2] TEST: [6 7 1 0]
+    TRAIN: [4 7 0 1 1] TEST: [5 3 6 5]
+    See also
+    --------
+    ShuffleSplit: cross validation using random permutations.
+    """
+
+    # Static marker to be able to introspect the CV type
+    indices = True
+
+    def __init__(self, n, n_bootstraps=3, train_size=.5, test_size=None,
+                 n_train=None, n_test=None, random_state=0):
+        self.n = n
+        self.n_bootstraps = n_bootstraps
+        if n_train is not None:
+            train_size = n_train
+            warnings.warn(
+                "n_train is deprecated in 0.11 and scheduled for "
+                "removal in 0.12, use train_size instead",
+                DeprecationWarning, stacklevel=2)
+        if n_test is not None:
+            test_size = n_test
+            warnings.warn(
+                "n_test is deprecated in 0.11 and scheduled for "
+                "removal in 0.12, use test_size instead",
+                DeprecationWarning, stacklevel=2)
+        if (isinstance(train_size, float) and train_size >= 0.0
+                            and train_size <= 1.0):
+            self.train_size = ceil(train_size * n)
+        elif isinstance(train_size, int):
+            self.train_size = train_size
+        else:
+            raise ValueError("Invalid value for train_size: %r" %
+                             train_size)
+        if self.train_size > n:
+            raise ValueError("train_size=%d should not be larger than n=%d" %
+                             (self.train_size, n))
+
+        if (isinstance(test_size, float) and test_size >= 0.0
+                    and test_size <= 1.0):
+            self.test_size = ceil(test_size * n)
+        elif isinstance(test_size, int):
+            self.test_size = test_size
+        elif test_size is None:
+            self.test_size = self.n - self.train_size
+        else:
+            raise ValueError("Invalid value for test_size: %r" % test_size)
+        if self.test_size > n:
+            raise ValueError("test_size=%d should not be larger than n=%d" %
+                             (self.test_size, n))
+
+        self.random_state = random_state
+
+    def __iter__(self):
+        rng = check_random_state(self.random_state)
+        for i in range(self.n_bootstraps):
+            # random partition
+            permutation = rng.permutation(self.n)
+            ind_train = permutation[:self.train_size]
+            ind_test = permutation[self.train_size:self.train_size
+                                   + self.test_size]
+
+            # bootstrap in each split individually
+            train = rng.randint(0, self.train_size,
+                                size=(self.train_size,))
+            test = rng.randint(0, self.test_size,
+                                size=(self.test_size,))
+            yield ind_train[train], ind_test[test]
+
+    def __repr__(self):
+        return ('%s(%d, n_bootstraps=%d, train_size=%d, test_size=%d, '
+                'random_state=%d)' % (
+                    self.__class__.__name__,
+                    self.n,
+                    self.n_bootstraps,
+                    self.train_size,
+                    self.test_size,
+                    self.random_state,
+                ))
+
+    def __len__(self):
+        return self.n_bootstraps
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self.__len__()
+
+    def split(self, X, y, groups=None):
+        indices = range(X.shape[0])
+        split = list()
+        for trains, tests in self:
+            split.append((trains.tolist(), tests.tolist()))
+        return split
+
 name_to_constructor = {
     # sklearn splitters:
+    'Bootstrap': Bootstrap,
     'GroupKFold': ms.GroupKFold,
     'GroupShuffleSplit': ms.GroupShuffleSplit,
     'KFold': ms.KFold,
