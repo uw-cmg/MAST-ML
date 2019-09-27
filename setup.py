5b02e308f6f11fa637d0eaf2cadc48093be233b3
rjacobs914 <rjacobs3@wisc.edu>
2019-09-27 15:20:09 -0500

    package correction to setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ except EnvironmentError:
 
 setup(
     name="mastml", # TODO  should this be MAST-ML?
-    packages=['mastml', 'mastml.legos', 'mastml.magpie', 'mastml.tests.conf', 'mastml.test.csv'],
+    packages=['mastml', 'mastml.legos', 'mastml.magpie', 'mastml.tests.conf', 'mastml.tests.csv'],
     package_data={'mastml.magpie': ["*.*"], 'mastml.tests': ["*.*"], 'mastml.tests.conf' : ["example_input.conf", "MASTML_fullinputfile.conf"], 'mastml.tests.csv' : ["example_data.csv"]},
     include_package_data = True,
     version=verstr,
