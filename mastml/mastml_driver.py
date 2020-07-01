c71277f668f9d845f89d4a98e283728691a90dc2
rjacobs914 <rjacobs3@wisc.edu>
2020-07-01 12:23:05 -0500

    fix to setup for ensemblemodel

--- mastml/mastml_driver.py
+++ mastml/mastml_driver.py
@@ -960,7 +960,8 @@ def mastml_run(conf_path, data_path, outdir):
         for split_num, (train_indices, test_indices) in enumerate(trains_tests):
 
             path = join(main_path, f"split_{split_num}")
-            models['EnsembleRegressor'].setup(path)
+            if 'EnsembleRegressor' in model.__class__.__name__:
+                models['EnsembleRegressor'].setup(path)
 
             split_results.append(one_fit(split_num, train_indices, test_indices, normalizer_instance))
 
