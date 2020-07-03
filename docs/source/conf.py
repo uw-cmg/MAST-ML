dd57082458a82dbd8369c3251e40d856a1f794cb
rjacobs914 <rjacobs3@wisc.edu>
2020-07-03 09:01:52 -0500

    doc update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,14 +88,15 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'classic'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'collapse_navigation': False}
+# Works for sphinx_rtd_theme
+#html_theme_options = {'collapse_navigation': False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
