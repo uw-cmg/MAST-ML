d637aee3eb371ba8adc6a26beca01a83bca6c57e
rjacobs914 <rjacobs3@wisc.edu>
2021-01-04 09:26:41 -0600

    update to imputer

--- mastml/data_cleaner.py
+++ mastml/data_cleaner.py
@@ -78,11 +78,11 @@ def imputation(df, strategy, cols_to_leave_out=None):
     """
     col_names = df.columns.tolist()
     if cols_to_leave_out is None:
-        df_imputed = pd.DataFrame(Imputer(missing_values='NaN', strategy=strategy, axis=0).fit_transform(df))
+        df_imputed = pd.DataFrame(Imputer(missing_values=np.nan, strategy=strategy).fit_transform(df))
     else:
         df_include = df.drop(cols_to_leave_out, axis=1)
         df_hold_out = df.drop([c for c in df.columns if c not in cols_to_leave_out], axis=1)
-        df_imputed = pd.DataFrame(Imputer(missing_values='NaN', strategy=strategy, axis=0).fit_transform(df_include), columns=df_include.columns)
+        df_imputed = pd.DataFrame(Imputer(missing_values=np.nan, strategy=strategy).fit_transform(df_include), columns=df_include.columns)
     # Need to join the imputed dataframe with the columns containing strings that were held out
     if cols_to_leave_out is None:
         df = df_imputed
