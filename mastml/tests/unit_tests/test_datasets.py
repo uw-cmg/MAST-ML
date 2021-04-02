f99f972b31dc4568d2f9c23b3550e38fcc939ffe
rjacobs914 <rjacobs3@wisc.edu>
2021-04-02 09:15:29 -0500

    moving data_cleaning unit tests to separate file

--- mastml/tests/unit_tests/test_datasets.py
+++ mastml/tests/unit_tests/test_datasets.py
@@ -6,7 +6,7 @@ import sys
 import shutil
 sys.path.insert(0, os.path.abspath('../../../'))
 
-from mastml.datasets import SklearnDatasets, LocalDatasets, FoundryDatasets, DataCleaning, MatminerDatasets
+from mastml.datasets import SklearnDatasets, LocalDatasets, FoundryDatasets, MatminerDatasets
 
 class TestDatasets(unittest.TestCase):
 
@@ -63,24 +63,6 @@ class TestDatasets(unittest.TestCase):
         foundrydata = FoundryDatasets(no_local_server=False, anonymous=True, test=True)
         foundrydata.download_data(name='pub_57_wu_highthroughput', download=False)
         return
-
-
-class TestDataCleaning(unittest.TestCase):
-
-    def test_datacleaning(self):
-        X = pd.DataFrame(np.random.uniform(low=0.0, high=100, size=(50, 10)))
-        y_input = pd.Series(np.random.uniform(low=0.0, high=100, size=(50,)))
-        y_input.name = 'target'
-        X_input = X.mask(np.random.random(X.shape) < 0.1)
-        DataCleaning().remove(X=X_input, y=y_input, axis=1)
-        DataCleaning().imputation(X=X_input, y=y_input, strategy='mean')
-        DataCleaning().ppca(X=X_input, y=y_input)
-        cleaner = DataCleaning()
-        cleaner.evaluate(X=X_input, y=y_input, method='remove', axis=1, savepath=os.getcwd())
-        self.assertTrue(os.path.exists(cleaner.splitdir))
-        shutil.rmtree(cleaner.splitdir)
-        return
-
 
 if __name__ == '__main__':
     unittest.main()
