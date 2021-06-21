c1deaee8487ab7013e96b3fe1da4cf654acbff0c
Hock Lye Kee <hlkee@outlook.com>
2021-06-21 14:14:01 -0500

    Add files via upload

--- mastml/tests/unit_tests/test_baseline_tests.py
+++ mastml/tests/unit_tests/test_baseline_tests.py
@@ -1,21 +1,16 @@
 import unittest
-import numpy as np
-import pandas as pd
 import os
 import sys
-
-from mastml.data_splitters import SklearnDataSplitter
 from mastml.datasets import LocalDatasets
 from mastml.models import SklearnModel
 from mastml.preprocessing import SklearnPreprocessor
 from mastml.baseline_tests import Baseline_tests
 
-sys.path.insert(0, os.path.abspath('../../../'))
-
 from mastml.feature_selectors import NoSelect, EnsembleModelFeatureSelector, PearsonSelector, MASTMLFeatureSelector
 from sklearn.ensemble import RandomForestRegressor
 
 class test_baseline(unittest.TestCase):
+
     def test_baseline_mean(self):
         target = 'E_regression.1'
         extra_columns = ['Material compositions 1', 'Material compositions 2', 'Hop activation barrier', 'E_regression']
@@ -27,20 +22,55 @@ class test_baseline(unittest.TestCase):
         data_dict = d.load_data()
         X = data_dict['X']
         y = data_dict['y']
-        # model = SklearnModel(model='RandomForestRegressor', n_estimators=150)
-        # preprocessor = SklearnPreprocessor(preprocessor='StandardScaler', as_frame=True)
-
-        # X = pd.DataFrame(np.random.uniform(low=0.0, high=100, size=(50, 5)))
-        # y = pd.Series(np.random.uniform(low=0.0, high=100, size=(50,)))
+        preprocessor = SklearnPreprocessor(preprocessor='StandardScaler', as_frame=True)
         model = SklearnModel(model='LinearRegression')
+        X = preprocessor.evaluate(X,y)
         model.fit(X=X, y=y)
 
         baseline = Baseline_tests()
         baseline.test_mean(X=X, y=y , model=model)
         return
 
-        # test_mean = baseline_tests()
-        # test_mean.test_mean(X,y)
+    def test_baseline_permute(self):
+        target = 'E_regression.1'
+        extra_columns = ['Material compositions 1', 'Material compositions 2', 'Hop activation barrier', 'E_regression']
+        d = LocalDatasets(file_path='mastml/data/figshare_7418492/All_Model_Data.xlsx',
+                          target=target,
+                          extra_columns=extra_columns,
+                          group_column='Material compositions 1',
+                          as_frame=True)
+        data_dict = d.load_data()
+        X = data_dict['X']
+        y = data_dict['y']
+        preprocessor = SklearnPreprocessor(preprocessor='StandardScaler', as_frame=True)
+        model = SklearnModel(model='LinearRegression')
+        X = preprocessor.evaluate(X,y)
+        model.fit(X=X, y=y)
+
+        baseline = Baseline_tests()
+        baseline.test_permuted(X=X, y=y , model=model)
+        return
+
+    def test_baseline_nearest_neighbor(self):
+        target = 'E_regression.1'
+        extra_columns = ['Material compositions 1', 'Material compositions 2', 'Hop activation barrier', 'E_regression']
+        d = LocalDatasets(file_path='mastml/data/figshare_7418492/All_Model_Data.xlsx',
+                          target=target,
+                          extra_columns=extra_columns,
+                          group_column='Material compositions 1',
+                          as_frame=True)
+        data_dict = d.load_data()
+        X = data_dict['X']
+        y = data_dict['y']
+        preprocessor = SklearnPreprocessor(preprocessor='StandardScaler', as_frame=True)
+        model = SklearnModel(model='LinearRegression')
+        X = preprocessor.evaluate(X,y)
+        model.fit(X=X, y=y)
+
+        baseline = Baseline_tests()
+        baseline.test_nearest_neighbour(X=X, y=y, model=model)
+        return
+
 
 if __name__=='__main__':
     unittest.main()
