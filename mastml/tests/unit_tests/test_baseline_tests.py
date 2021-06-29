eb86de691bb3142d1f0a36cd9af69c2e44f069e7
Hock Lye Kee <hlkee@outlook.com>
2021-06-29 15:41:41 -0500

    Add files via upload

--- mastml/tests/unit_tests/test_baseline_tests.py
+++ mastml/tests/unit_tests/test_baseline_tests.py
@@ -51,7 +51,7 @@ class test_baseline(unittest.TestCase):
         baseline.test_permuted(X=X, y=y , model=model)
         return
 
-    def test_baseline_nearest_neighbor(self):
+    def test_baseline_nearest_neighbor_kdTree(self):
         target = 'E_regression.1'
         extra_columns = ['Material compositions 1', 'Material compositions 2', 'Hop activation barrier', 'E_regression']
         d = LocalDatasets(file_path='mastml/data/figshare_7418492/All_Model_Data.xlsx',
@@ -68,7 +68,27 @@ class test_baseline(unittest.TestCase):
         model.fit(X=X, y=y)
 
         baseline = Baseline_tests()
-        baseline.test_nearest_neighbour(X=X, y=y, model=model)
+        baseline.test_nearest_neighbour_kdtree(X=X, y=y, model=model)
+        return
+
+    def test_baseline_nearest_neighbor_cdist(self):
+        target = 'E_regression.1'
+        extra_columns = ['Material compositions 1', 'Material compositions 2', 'Hop activation barrier', 'E_regression']
+        d = LocalDatasets(file_path='mastml/data/figshare_7418492/All_Model_Data.xlsx',
+                          target=target,
+                          extra_columns=extra_columns,
+                          group_column='Material compositions 1',
+                          as_frame=True)
+        data_dict = d.load_data()
+        X = data_dict['X']
+        y = data_dict['y']
+        preprocessor = SklearnPreprocessor(preprocessor='StandardScaler', as_frame=True)
+        model = SklearnModel(model='LinearRegression')
+        X = preprocessor.evaluate(X,y)
+        model.fit(X=X, y=y)
+
+        baseline = Baseline_tests()
+        baseline.test_nearest_neighbour_cdist(X=X, y=y, model=model, d_metric="euclidean")
         return
 
 
