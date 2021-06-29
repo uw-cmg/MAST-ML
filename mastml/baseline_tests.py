efb13d53c33fb4ab34c278a24544a0ff5230c650
Hock Lye Kee <hlkee@outlook.com>
2021-06-29 15:41:13 -0500

    Wrote scipy.spatial.distance.cdist for NN test

--- mastml/baseline_tests.py
+++ mastml/baseline_tests.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 import scipy as sp
+from scipy.spatial.distance import cdist
 import mastml
 from mastml.data_splitters import SklearnDataSplitter
 from mastml.metrics import Metrics
@@ -33,13 +34,27 @@ class Baseline_tests():
 
                 metrics: (list), list of metric names to evaluate true vs. pred data in each split
 
-        test_mean: Compares the score of the model with the test value of the nearest neighbour
+        test_nearest_neighbour_kdTree: Compares the score of the model with the test value of the nearest neighbour found using kdTree
             Args:
                 X: (dataframe), dataframe of X features
 
                 y: (dataframe), dataframe of y data
 
                 metrics: (list), list of metric names to evaluate true vs. pred data in each split
+
+        test_nearest_neighbour_kdTree: Compares the score of the model with the test value of the nearest neighbour found using cdist
+            Args:
+                X: (dataframe), dataframe of X features
+
+                y: (dataframe), dataframe of y data
+
+                metrics: (list), list of metric names to evaluate true vs. pred data in each split
+
+        print_results: Prints the comparison between the naive score and the real score
+            Args:
+                real_score: The actual score of the model
+
+                naive_score: The naive score of the model tested with fake_test
 
     '''
 
@@ -61,13 +76,9 @@ class Baseline_tests():
         y_predict = model.predict(X_test)
 
         real_score = Metrics(metrics_list=metrics).evaluate(y_true=y_test, y_pred=y_predict)
-
         naive_score = Metrics(metrics_list=metrics).evaluate(y_true=fake_test, y_pred=y_predict)
+        self.print_results(real_score,naive_score)
 
-        for (k, v), (k2, v2) in zip(real_score.items(), naive_score.items()):
-            print(k, "score:")
-            print("Real -", v)
-            print("Fake -", v2, "\n")
         return
 
     def test_permuted(self, X, y, model, metrics=["mean_absolute_error"]):
@@ -84,16 +95,12 @@ class Baseline_tests():
         y_predict = model.predict(X_test)
 
         real_score = Metrics(metrics_list=metrics).evaluate(y_true=y_test, y_pred=y_predict)
-
         naive_score = Metrics(metrics_list=metrics).evaluate(y_true=fake_test, y_pred=y_predict)
+        self.print_results(real_score,naive_score)
 
-        for (k, v), (k2, v2) in zip(real_score.items(), naive_score.items()):
-            print(k, "score:")
-            print("Real -", v)
-            print("Fake -", v2, "\n")
         return
 
-    def test_nearest_neighbour(self, X, y , model, metrics = ["mean_absolute_error"]):
+    def test_nearest_neighbour_kdtree(self, X, y , model, metrics = ["mean_absolute_error"]):
         splitter = SklearnDataSplitter(splitter='RepeatedKFold', n_repeats=1, n_splits=5)
         X_splits, y_splits, train_inds, test_inds = splitter.split_asframe(X, y)
         for Xs, ys, train_ind, test_ind in zip(X_splits, y_splits, train_inds, test_inds):
@@ -112,11 +119,39 @@ class Baseline_tests():
         y_predict = model.predict(X_test)
 
         real_score = Metrics(metrics_list=metrics).evaluate(y_true=y_test, y_pred=y_predict)
-
         naive_score = Metrics(metrics_list=metrics).evaluate(y_true=fake_test, y_pred=y_predict)
+        self.print_results(real_score,naive_score)
 
+        return
+
+    def test_nearest_neighbour_cdist(self, X, y , model, metrics = ["mean_absolute_error"], d_metric = None):
+        splitter = SklearnDataSplitter(splitter='RepeatedKFold', n_repeats=1, n_splits=5)
+        X_splits, y_splits, train_inds, test_inds = splitter.split_asframe(X, y)
+        for Xs, ys, train_ind, test_ind in zip(X_splits, y_splits, train_inds, test_inds):
+            X_train = Xs[0]
+            X_test = Xs[1]
+            y_train = pd.Series(np.array(ys[0]).ravel(), name='y_train')
+            y_test = pd.Series(np.array(ys[1]).ravel(), name='y_test')
+
+        result = cdist(X_test, X_train, metric = d_metric)
+        fake_test = []
+
+        #Get the index of the nearest neighbour (i.e shortest distance) from the result
+        for i in range(len(X_test)):
+            nn_index = result[i].tolist().index(result[i].min())
+            # Use the nearest neighbour datapoint's y_test instead of the actual y_test
+            fake_test.append(y_train[nn_index])
+
+        y_predict = model.predict(X_test)
+
+        real_score = Metrics(metrics_list=metrics).evaluate(y_true=y_test, y_pred=y_predict)
+        naive_score = Metrics(metrics_list=metrics).evaluate(y_true=fake_test, y_pred=y_predict)
+        self.print_results(real_score,naive_score)
+
+        return
+
+    def print_results(self, real_score, naive_score):
         for (k,v), (k2,v2) in zip(real_score.items(), naive_score.items()):
             print(k , "score:")
-            print("Real -", v)
-            print("Fake -", v2, "\n")
-        return
+            print("Real:", v)
+            print("Fake:", v2, "\n")
