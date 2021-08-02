bd7e75ed348ea2e8cde1f21acf49a30f1e6babb3
leschultz <lsschultz@wisc.edu>
2021-08-02 11:53:48 -0500

    Removed tqdm for reduction of print to screen material

--- mastml/functions.py
+++ mastml/functions.py
@@ -1,6 +1,5 @@
 from pathos.multiprocessing import ProcessingPool as Pool
 from functools import partial
-from tqdm import tqdm
 
 import sys
 import os
@@ -22,10 +21,6 @@ def parallel(func, x, *args, **kwargs):
     part_func = partial(func, *args, **kwargs)
 
     with Pool(os.cpu_count()) as pool:
-        data = list(tqdm(
-                         pool.imap(part_func, x),
-                         total=len(x),
-                         file=sys.stdout)
-                         )
+        data = list(pool.imap(part_func, x))
 
     return data
