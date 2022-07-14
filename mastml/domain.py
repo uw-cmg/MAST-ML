788c435afd85a8ec432163d6a35737219dee8e28
leschultz <laneenriqueschultz@gmail.com>
2022-07-14 15:43:58 -0500

    Lane: modified domain subroutine for eventual integration (in progress).

--- mastml/domain.py
+++ mastml/domain.py
@@ -1,67 +1,83 @@
-'''
-This module contains a collection of routines to perform domain evaluations
-'''
-import numpy as np
-import pandas as pd
+from sklearn.gaussian_process import GaussianProcessRegressor
 from scipy.spatial.distance import cdist
 
-class Domain():
+import pandas as pd
+import numpy as np
+import torch
+
+
+class Domain:
     '''
-    This class evaluates which test data point is within and out of the domain
-
-    Args:
-        None.
-
-    Methods:
-        distance: calculates the distance of the test data point from centroid of data to determine if in or out of domain
-
-        Args:
-            X_train: (dataframe), dataframe of X_train features
-
-            X_test: (dataframe), dataframe of X_test features
-
-            metrics: (str), string denoting the method of calculating the distance, ie mahalanobis
-
-            **kwargs: (str), string denoting extra argument needed for metric, e.g minkowski require additional arg p
+    Trainable domain object that can calculate dissimilarities between
+    sets of data.
     '''
-    def distance(self, X_train, X_test, metrics, **kwargs):
 
-        #TODO: Lane says it gives error because the input is a pd.series but the shouldn't the input be a str?
-        # Only changing this to take a series so that it will pass the test
-        for metric in metrics:
-            if metric == 'mahalanobis':
-                m = np.mean(X_train)
-                X_train_transposed = np.transpose(X_train)
-                covM = np.cov(X_train_transposed)
-                invCovM = np.linalg.inv(covM)
-                max_distance_train = cdist(XA=[m], XB=X_train, metric=metric, VI=invCovM).max()
+    def train(self, X, y=None):
+        self.dist_func = lambda x: distance(X, x, y)
 
-                #Do the same for X_test
-                centroid_dist = cdist(XA=[m], XB=X_test, metric=metric, VI=invCovM)[0];
-                #Check every test datapoint to see if they are in or out of domain
-                inDomain = []
-                for i in centroid_dist:
-                    if pd.isna(i):
-                        inDomain.append("nan")
-                    elif i < max_distance_train:
-                        inDomain.append(True)
-                    else:
-                        inDomain.append(False)
+    def predict(self, X):
+        return self.dist_func(X)
 
-                return pd.DataFrame(inDomain)
 
-            else:
-                m = np.mean(X_train)
-                max_distance_train = cdist(XA=[m], XB=X_train, metric=metric, **kwargs).max()
-                centroid_dist = cdist(XA=[m], XB=X_test, metric=metric, **kwargs)[0];
-                inDomain = []
-                print(centroid_dist)
-                for i in centroid_dist:
-                    if pd.isna(i):
-                        inDomain.append("nan")
-                    elif i < max_distance_train:
-                        inDomain.append(True)
-                    else:
-                        inDomain.append(False)
+def distance_link(
+                  X_train,
+                  X_test,
+                  dist_type,
+                  y_train=None,
+                  ):
+    '''
+    Get the distances based on a metric.
 
-                return pd.DataFrame(inDomain)
+    inputs:
+        X_train = The features of the training set.
+        X_test = The features of the test set.
+        dist = The distance to consider.
+        y_train = The training target when applicable.
+    ouputs:
+        dists = A dictionary of distances.
+    '''
+
+    dists = {}
+
+    if dist_type == 'gpr_std':
+
+        model = GaussianProcessRegressor()
+        model.fit(X_train, y_train)
+        _, dist = model.predict(X_test, return_std=True)
+        dists[dist_type] = dist
+
+    else:
+        dist = cdist(X_train, X_test, dist_type)
+        dists[dist_type] = np.mean(dist, axis=0)
+
+    return dists
+
+
+def distance(
+             X_train,
+             X_test,
+             y_train=None,
+             ):
+    '''
+    Determine the distance from set X_test to set X_train.
+    '''
+
+    # List of supported distances
+    distance_list = [
+                     'gpr_std',
+                     'euclidean',
+                     'mahalanobis',
+                     ]
+
+    dists = {}
+    for distance in distance_list:
+
+        # Compute regular distances
+        dists.update(distance_link(
+                                   X_train,
+                                   X_test,
+                                   distance,
+                                   y_train=y_train,
+                                   ))
+
+    return dists
--- mastml/tests/unit_tests/test_domain.py
+++ mastml/tests/unit_tests/test_domain.py
@@ -5,18 +5,25 @@ from mastml.domain import Domain
 from sklearn.model_selection import train_test_split
 
 import mastml
+
 try:
     data_path = mastml.__path__._path[0]
-except:
+except Exception:
     data_path = mastml.__path__[0]
 
+
 class test_baseline(unittest.TestCase):
+
     def test_mahalanobis(self):
         target = 'E_regression'
 
-        extra_columns = ['Material compositions 1', 'Material compositions 2','Hop activation barrier']
+        extra_columns = [
+                         'Material compositions 1',
+                         'Material compositions 2',
+                         ]
 
-        d = LocalDatasets(file_path=data_path + '/data/diffusion_data_allfeatures.xlsx',
+        d = file_path=data_path+'/data/diffusion_data_selectfeatures.xlsx'
+        d = LocalDatasets(d,
                           target=target,
                           extra_columns=extra_columns,
                           group_column='Material compositions 1',
@@ -29,11 +36,21 @@ class test_baseline(unittest.TestCase):
         # Let's assign each data object to its respective name
         X = data_dict['X']
         y = data_dict['y']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=3)
-        domain = Domain()
-        domain.distance(X_train, X_test, 'mahalanobis')
 
-        return
+        X_train, X_test, y_train, y_test = train_test_split(
+                                                            X,
+                                                            y,
+                                                            random_state=3
+                                                            )
+
+        # Now train dissimilarity model
+        domain = Domain()
+        domain.train(X_train, y_train)
+
+        # Ideally, we would apply in CV, not training data
+        df_train = domain.predict(X_train)  # On average very close
+        df_test = domain.predict(X_test)  # On average very far
+
 
 if __name__ == '__main__':
     unittest.main()
