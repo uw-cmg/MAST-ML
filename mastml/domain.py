4957f5fd3f76ce9fef0d8ff44e92e70e96f1e9ef
leschultz <laneenriqueschultz@gmail.com>
2022-07-18 12:25:45 -0500

    Now applying domain analysis post nested CV

--- mastml/domain.py
+++ mastml/domain.py
@@ -1,12 +1,50 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
 from scipy.spatial.distance import cdist
+from mastml.mastml import parallel
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
-import torch
+import os
 
 
 class Domain:
+    '''
+    Trainable domain object that can calculate dissimilarities between
+    sets of data.
+    '''
+
+    def __init__(self, path, parallel_run=True):
+        paths = list(map(str, Path(path).rglob('*X_train*')))
+
+        def calculate(path):
+
+            splits = path.split('/')
+            path = '/'.join(splits[:-1])
+
+            X_train = pd.read_csv(path+'/X_train.csv')
+            y_train = pd.read_csv(path+'/y_train.csv')
+            X_test = pd.read_csv(path+'/X_test.csv')
+
+            # Cannot measure non-numeric data (may consider one-hot encoding)
+            X_train = X_train._get_numeric_data()
+            y_train = y_train.values
+            X_test = X_test._get_numeric_data()
+
+            domain = domain_split()
+            domain.train(X_train, y_train)
+            dist = domain.predict(X_test)
+            dist = pd.DataFrame(dist)
+            dist.to_csv(os.path.join(path, 'dist.csv'), index=False)
+
+        if parallel_run is True:
+            parallel(calculate, paths)
+        else:
+            [calculate(i) for i in paths]
+
+
+
+class domain_split:
     '''
     Trainable domain object that can calculate dissimilarities between
     sets of data.
