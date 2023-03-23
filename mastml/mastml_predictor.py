35d38b3371739ee02fadf7268913af0e9930c5cd
rjacobs914 <rjacobs3@wisc.edu>
2023-03-23 18:22:32 -0500

    made updated make_prediction_dlhub function to coincide with updated make_prediction function

--- mastml/mastml_predictor.py
+++ mastml/mastml_predictor.py
@@ -174,6 +174,144 @@ def make_prediction(X_test, X_train, y_train, model, preprocessor=None, calibrat
 
 def make_prediction_dlhub(input_dict):
     '''
+    Method used to take a saved preprocessor, model and calibration file and output predictions and calibrated uncertainties
+    on new test data
+
+    Args:
+        input_dict: (dict), dictionary of input passed to predictor. The dictionary may have the following keys:
+
+            X_test: (pd.DataFrame or str), dataframe of featurized test data to be used to make prediction, or string of path
+                containing featurized test data in .xlsx or .csv format ready for import with pandas. If passing an already
+                featurized dataframe, only the features used to fit the original model should be included, and they should be
+                in the same order as the training data used to fit the original model.
+
+            featurizers: (list), list of strings denoting paths to saved mastml feature generators, e.g., ["myfolder/ElementalFeatureGenerator.pkl", "myfolder/PolynomialFeatureGenerator.pkl"]
+
+            featurize_on: (list), list of strings of column name in X_test to perform featurization on, needs to be same length and in
+                same order as featurizers listed above, e.g., ['Composition', ['feature1', 'feature2'] ]
+
+    Returns:
+        pred_df: (pd.DataFrame), dataframe containing column of model predictions (y_pred) and, if applicable, calibrated uncertainties (y_err).
+            Will also include any extra columns denoted in extra_columns parameter.
+    '''
+
+    # Load model:
+    model = joblib.load('model.pkl')
+
+    # Check if recalibration params exist:
+    if os.path.exists('calibration_file.xlsx'):
+         recal_params = pd.read_excel(os.path.join(os.getcwd(), 'calibration_file.xlsx'), engine='openpyxl')
+    elif os.path.exists('calibration_file.csv'):
+        recal_params = pd.read_csv(os.path.join(os.getcwd(), 'calibration_file.csv'))
+    else:
+        recal_params = None
+
+    # Load in the X_test data
+    X_test = input_dict['X_test']
+
+    # Load in the X_train data
+    if os.path.exists('X_train.xlsx'):
+        X_train = pd.read_excel('X_train.xlsx', engine='openpyxl')
+    elif os.path.exists('X_train.csv'):
+        X_train = pd.read_csv('X_train.csv')
+
+    features_to_keep = X_train.columns.tolist()
+
+    # Load in the y_train data
+    if os.path.exists('y_train.xlsx'):
+        y_train = pd.read_excel('y_train.xlsx', engine='openpyxl')
+    elif os.path.exists('y_train.csv'):
+        y_train = pd.read_csv('y_train.csv')
+
+    # Load featurizers
+    featurizers = input_dict['featurizers']
+    featurize_on = input_dict['featurize_on']
+    df_test = X_test
+    if featurizers is not None:
+        # Load in the featurizers
+        for f, f_on in zip(featurizers, featurize_on):
+            try:
+                gen = joblib.load(f+'.pkl')
+                print('generator', gen)
+            except:
+                gen = joblib.load(f)
+            gen.featurize_df = pd.DataFrame(X_test[f_on])
+            df_test, _ = gen.evaluate(X=df_test, y=pd.Series(np.zeros(shape=df_test.shape[0])), savepath=None, make_new_dir=False)
+        df_test = df_test[features_to_keep]
+
+    # Check if any of the featurized rows are in the training data. If so, append the true target value
+    y_true_list = list()
+    for i, vals_i in enumerate(df_test[features_to_keep].iterrows()):
+        found = False
+        for j, vals_j in enumerate(X_train[features_to_keep].iterrows()):
+            if vals_i[1].round(6).equals(vals_j[1].round(6)):
+                y_true_list.append(np.array(y_train)[j][0])
+                found = True
+                break
+        if found == False:
+            y_true_list.append(np.nan)
+
+    # Load preprocessor
+    if os.path.exists('preprocessor.pkl'):
+        preprocessor = joblib.load('preprocessor.pkl')
+        df_test = preprocessor.transform(df_test)
+
+    # Check the model is an ensemble and get an error bar:
+    ensemble_models = ['RandomForestRegressor',
+                       'GradientBoostingRegressor',
+                       'BaggingRegressor',
+                       'ExtraTreesRegressor',
+                       'AdaBoostRegressor']
+    try:
+        model_name = model.model.__class__.__name__
+    except:
+        model_name = model.__class__
+    yerr = list()
+    if model_name in ensemble_models:
+        X_aslist = df_test.values.tolist()
+        for x in range(len(X_aslist)):
+            preds = list()
+            if model_name == 'RandomForestRegressor':
+                for pred in model.model.estimators_:
+                    preds.append(pred.predict(np.array(X_aslist[x]).reshape(1, -1))[0])
+            elif model_name == 'BaggingRegressor':
+                for pred in model.model.estimators_:
+                    preds.append(pred.predict(np.array(X_aslist[x]).reshape(1, -1))[0])
+            elif model_name == 'ExtraTreesRegressor':
+                for pred in model.model.estimators_:
+                    preds.append(pred.predict(np.array(X_aslist[x]).reshape(1, -1))[0])
+            elif model_name == 'GradientBoostingRegressor':
+                for pred in model.model.estimators_.tolist():
+                    preds.append(pred[0].predict(np.array(X_aslist[x]).reshape(1, -1))[0])
+            elif model_name == 'AdaBoostRegressor':
+                for pred in model.model.estimators_:
+                    preds.append(pred.predict(np.array(X_aslist[x]).reshape(1, -1))[0])
+            if recal_params is not None:
+                yerr.append(recal_params['a'][0]*np.std(preds)+recal_params['b'][0])
+            else:
+                yerr.append(np.std(preds))
+
+    if model_name == 'GaussianProcessRegressor':
+        y_pred_new, yerr = model.model.predict(df_test, return_std=True)
+    else:
+        y_pred_new = model.predict(df_test)
+
+    if len(yerr) > 0:
+        pred_df = pd.DataFrame(y_pred_new, columns=['y_pred'])
+        pred_df['y_err'] = yerr
+    else:
+        pred_df = pd.DataFrame(y_pred_new, columns=['y_pred'])
+
+    for col in X_test.columns.tolist():
+        pred_df[col] = X_test[col]
+
+    # Add the y_true column into the predicted dataframe:
+    pred_df['y_true'] = y_true_list
+
+    return pred_df
+
+def make_prediction_dlhub_OLD(input_dict):
+    '''
     Prediction script, same functionality as make_prediction above, but tailored for model running on DLHub/Foundry
 
     Use this function as the function pointer for DLHub PythonStaticMethodModel (see 'Foundry_model_upload_example.ipynb'
